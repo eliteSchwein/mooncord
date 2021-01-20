67b7a29b5069d5cebbd2fd36ffd5da4d67b02cb9
eliteSCHW31N <39838619+eliteSchwein@users.noreply.github.com>
2021-01-20 21:47:10 +0100

    add primitive help discord command

--- discord-commands/help.js
+++ discord-commands/help.js
@@ -2,9 +2,28 @@ const config = require('../config.json');
 const admin = false
 const master = false
 const discordDatabase = require('../discorddatabase')
+const Discord = require('discord.js');
 var fs = require('fs');
+var commands = ""
 var executeCommand = (function(command,channel,user,guild,discordClient,websocketClient){
-    channel.send("You found the template Command Module GG")
+    channel.startTyping();
+    commands = ""
+    fs.readdir(__dirname+"/", (err, files) => {
+        files.forEach(file => {
+            commands=commands.concat(" `"+file.replace(".js","")+"`")
+        });
+    });
+    setTimeout(function(){
+        const embed = new Discord.MessageEmbed()
+            .setColor('#0099ff')
+            .setAuthor('Help')
+            .setThumbnail(discordClient.user.avatarURL())
+            .setDescription('Aviable Commands:\n'+commands)
+            .setTimestamp()
+            .setFooter(user.tag, user.avatarURL());
+        channel.stopTyping()
+        channel.send(embed)
+    },2000)
 })
 module.exports = executeCommand;
 module.exports.needAdmin = function(){return admin}
