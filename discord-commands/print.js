46814bd6b6626f185b761dca06a83283c11b20cd
eliteSCHW31N <39838619+eliteSchwein@users.noreply.github.com>
2021-02-17 17:51:30 +0100

    use base64 directly

--- discord-commands/print.js
+++ discord-commands/print.js
@@ -57,7 +57,7 @@ async function handler(message){
             console.log(err)
         })
         var formData = new FormData();
-        formData.append('file',fs.createReadStream(__dirname+"/../temp/thumbnail.png"),"thumbnail_"+file+".png")
+        formData.append('file',thumbnail,"thumbnail_"+file+".png")
         axios
             .post('http://imagebin.ca/upload.php', formData,{
                 headers: formData.getHeaders()
