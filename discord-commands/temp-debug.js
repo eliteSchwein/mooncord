4f84ef0b6e92315f11e3bb0ae720d953edb84734
eliteSCHW31N <39838619+eliteSchwein@users.noreply.github.com>
2021-02-21 23:05:08 +0100

    update feedback

--- discord-commands/temp-debug.js
+++ discord-commands/temp-debug.js
@@ -4,15 +4,15 @@ const master = false
 const discordDatabase = require('../discorddatabase')
 var variables = require("../websocketevents")
 var executeCommand = (function(command,channel,user,guild,discordClient,websocketConnection){
-    var debug = "DEV COMMAND!\n"
+    var debug = ""
     var temps = variables.getTemps()
     for(var temp in temps){
         if(temp.includes("temperature_sensor")){
-            debug=debug.concat("🌡"+temp.replace("temperature_sensor ","")+":\n`"+temps[temp].temperatures[0]+"°C`\n\n")
+            debug=debug.concat("**🌡"+temp.replace("temperature_sensor ","")+":**\n`"+temps[temp].temperatures[0]+"°C`\n\n")
         }else if(temp.includes("extruder")||temp.includes("heater_bed")){
-            debug=debug.concat("♨"+temp+":\n`"+temps[temp].temperatures[0]+"/"+temps[temp].targets[0]+"°C` `"+temps[temp].powers[0]+"%`\n\n")
+            debug=debug.concat("**♨"+temp+":**\n`Current:"+temps[temp].temperatures[0]+"` `Target:"+temps[temp].targets[0]+"°C` `"+temps[temp].powers[0]+"%`\n\n")
         }else if(temp.includes("temperature_fan")){
-            debug=debug.concat("❄"+temp+":\n`"+temps[temp].temperatures[0]+"/"+temps[temp].targets[0]+"°C` `"+temps[temp].speeds[0]+"`\n\n")
+            debug=debug.concat("**❄"+temp+"**:\n`Current:"+temps[temp].temperatures[0]+"` `Target:"+temps[temp].targets[0]+"°C` `"+temps[temp].speeds[0]+"`\n\n")
         }
     }
     console.log(temps)
