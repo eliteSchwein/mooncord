b3a9052ab4814d2dfc939a9cab0e35f14991e843
eliteSCHW31N <39838619+eliteSchwein@users.noreply.github.com>
2021-02-21 23:13:42 +0100

    add experimentell percent calculator

--- discord-commands/temp-debug.js
+++ discord-commands/temp-debug.js
@@ -10,14 +10,18 @@ var executeCommand = (function(command,channel,user,guild,discordClient,websocke
         if(temp.includes("temperature_sensor")){
             debug=debug.concat("**🌡"+temp.replace("temperature_sensor ","")+":**\n`"+temps[temp].temperatures[temps[temp].temperatures.length-1]+"°C`\n\n")
         }else if(temp.includes("extruder")||temp.includes("heater_bed")){
-            debug=debug.concat("**♨"+temp+":**\n`Current:"+temps[temp].temperatures[temps[temp].temperatures.length-1]+"°C` `Target:"+temps[temp].targets[temps[temp].targets.length-1]+"°C` `Power:"+temps[temp].powers[temps[temp].powers.length-1]+"%`\n\n")
+            debug=debug.concat("**♨"+temp+":**\n`Current:"+temps[temp].temperatures[temps[temp].temperatures.length-1]+"°C` `Target:"+temps[temp].targets[temps[temp].targets.length-1]+"°C` `Power:"+calculatePercent(temps[temp].powers[temps[temp].powers.length-1])+"%`\n\n")
         }else if(temp.includes("temperature_fan")){
-            debug=debug.concat("**❄"+temp+"**:\n`Current:"+temps[temp].temperatures[temps[temp].temperatures.length-1]+"°C` `Target:"+temps[temp].targets[temps[temp].targets.length-1]+"°C` `Speed:"+temps[temp].speeds[temps[temp].speeds.length-1]+"`\n\n")
+            debug=debug.concat("**❄"+temp+"**:\n`Current:"+temps[temp].temperatures[temps[temp].temperatures.length-1]+"°C` `Target:"+temps[temp].targets[temps[temp].targets.length-1]+"°C` `Speed:"+calculatePercent(temps[temp].speeds[temps[temp].speeds.length-1])+"`\n\n")
         }
     }
     console.log(temps)
     channel.send(debug)
 })
+function calculatePercent(input){
+    var percent = 100*input
+    return percent.toFixed(0)
+}
 module.exports = executeCommand;
 module.exports.needAdmin = function(){return admin}
 module.exports.needMaster = function(){return master}
