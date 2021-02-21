8da14de9cbe83fba112f69fec18a48fba5f09e08
eliteSCHW31N <39838619+eliteSchwein@users.noreply.github.com>
2021-02-21 23:00:31 +0100

    update temp debug command

--- discord-commands/temp-debug.js
+++ discord-commands/temp-debug.js
@@ -7,9 +7,16 @@ var executeCommand = (function(command,channel,user,guild,discordClient,websocke
     var debug = "DEV COMMAND!\n"
     var temps = variables.getTemps()
     for(var temp in temps){
-        console.log(temp)
+        if(temp.includes("temperature_sensor")){
+            debug.concat("🌡"+temp.replace("temperature_sensor ","")+"\n`"+temps[temp].temperature[0]+"°C`\n")
+        }else if(temp.includes("extruder")||temp.includes("heater_bed")){
+            debug.concat("♨"+temp+"\n`"+temps[temp].temperature[0]+"/"+temps[temp].targets[0]+"°C` `"+temps[temp].powers[0]+"%`\n")
+        }else if(temp.includes("temperature_fan")){
+            debug.concat("❄"+temp+"\n`"+temps[temp].temperature[0]+"/"+temps[temp].targets[0]+"°C` `"+temps[temp].speeds[0]+"`\n")
+        }
     }
-    channel.send(JSON.stringify(variables.getTemps()))
+    console.log(temps)
+    channel.send(debug)
 })
 module.exports = executeCommand;
 module.exports.needAdmin = function(){return admin}
