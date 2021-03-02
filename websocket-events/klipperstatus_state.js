0f885a8636667babc9c3bac9c2f5f895f86b0d9b
eliteSCHW31N <39838619+eliteSchwein@users.noreply.github.com>
2021-03-02 10:08:41 +0100

    fix some checks

--- websocket-events/klipperstatus_state.js
+++ websocket-events/klipperstatus_state.js
@@ -12,52 +12,52 @@ var event = ((connection,discordClient) => {
                 if(typeof(result.version_info)!="undefined"){
                     variables.setVersions(result.version_info)
                 }
-            }
-            if(typeof(result.status)!="undefined"){
-                var klipperstatus = result.status;
-                if(typeof(klipperstatus.print_stats)!="undefined"){
-                    printfile=klipperstatus.print_stats.filename
-                    variables.setCurrentFile(printfile)
-                    var printduration = klipperstatus.print_stats.print_duration.toFixed(0)
-                    var remainingprinttime = printtime-printduration;
-                    restprinttime=formatDateTime(remainingprinttime*1000)
-                    if(klipperstatus.print_stats.state=="paused"){
-                        currentStatus="pause";
-                        if(variables.getStatus()!=currentStatus){
-                            variables.setStatus(currentStatus)
-                            variables.triggerStatusUpdate(discordClient)
-                            clearInterval(variables.getUpdateTimer())
-                        }
-                    }
-                    if(klipperstatus.print_stats.state=="printing"){
-                        if(typeof(printfile)!="undefined"||printfile!=""){
-                            currentStatus="printing";
-                            if(variables.getStatus()!=currentStatus){
-                            variables.setStatus(currentStatus)
-                                if(!config.statusupdatepercent){
-                                    variables.triggerStatusUpdate(discordClient)
-                                    setTimeout(function(){
-                                        timer=setInterval(function(){
-                                            variables.triggerStatusUpdate(discordClient)
-                                        },1000*config.statusupdateinterval)
-                                    },1000*config.statusupdateinterval)
-                                }else{
-                                    variables.triggerStatusUpdate(discordClient)
-                                }
-                            }
-                        }   
-                    }
-                    if(klipperstatus.print_stats.state=="complete"){
-                        if(variables.getStatus()!="ready"){
-                            currentStatus="done";
+                if(typeof(result.status)!="undefined"){
+                    var klipperstatus = result.status;
+                    if(typeof(klipperstatus.print_stats)!="undefined"){
+                        printfile=klipperstatus.print_stats.filename
+                        variables.setCurrentFile(printfile)
+                        var printduration = klipperstatus.print_stats.print_duration.toFixed(0)
+                        var remainingprinttime = printtime-printduration;
+                        restprinttime=formatDateTime(remainingprinttime*1000)
+                        if(klipperstatus.print_stats.state=="paused"){
+                            currentStatus="pause";
                             if(variables.getStatus()!=currentStatus){
                                 variables.setStatus(currentStatus)
                                 variables.triggerStatusUpdate(discordClient)
                                 clearInterval(variables.getUpdateTimer())
-                                setTimeout(function(){
-                                    variables.setStatus("ready")
-                                    triggerStatusUpdate(discordClient)
-                                },1000)
+                            }
+                        }
+                        if(klipperstatus.print_stats.state=="printing"){
+                            if(typeof(printfile)!="undefined"||printfile!=""){
+                                currentStatus="printing";
+                                if(variables.getStatus()!=currentStatus){
+                                variables.setStatus(currentStatus)
+                                    if(!config.statusupdatepercent){
+                                        variables.triggerStatusUpdate(discordClient)
+                                        setTimeout(function(){
+                                            timer=setInterval(function(){
+                                                variables.triggerStatusUpdate(discordClient)
+                                            },1000*config.statusupdateinterval)
+                                        },1000*config.statusupdateinterval)
+                                    }else{
+                                        variables.triggerStatusUpdate(discordClient)
+                                    }
+                                }
+                            }   
+                        }
+                        if(klipperstatus.print_stats.state=="complete"){
+                            if(variables.getStatus()!="ready"){
+                                currentStatus="done";
+                                if(variables.getStatus()!=currentStatus){
+                                    variables.setStatus(currentStatus)
+                                    variables.triggerStatusUpdate(discordClient)
+                                    clearInterval(variables.getUpdateTimer())
+                                    setTimeout(function(){
+                                        variables.setStatus("ready")
+                                        triggerStatusUpdate(discordClient)
+                                    },1000)
+                                }
                             }
                         }
                     }
--- websocket-events/klipperstatus_vsdcard.js
+++ websocket-events/klipperstatus_vsdcard.js
@@ -12,36 +12,36 @@ var event = ((connection,discordClient) => {
                 if(typeof(result.version_info)!="undefined"){
                     variables.setVersions(result.version_info)
                 }
-            }
-            if(typeof(result.status)!="undefined"){
-                var klipperstatus = result.status;
-                var currentProgress = 0
-                if(typeof(klipperstatus.virtual_sdcard)!="undefined"){
-                    if (klipperstatus.virtual_sdcard.file_position <= printstartbyte){
-                        currentProgress = 0
-                        variables.setProgress(currentProgress)
-                    }else if (klipperstatus.virtual_sdcard.file_position >= printendbyte){
-                        currentProgress = 100
-                        variables.setProgress(currentProgress)
-                    }else{
-                        let currentPosition = klipperstatus.virtual_sdcard.file_position - printstartbyte;
-                        let maxPosition = printendbyte - printstartbyte;
-                        if(currentPosition > 0 && maxPosition > 0){
-                            currentProgress = (1/maxPosition*currentPosition)*100
+                if(typeof(result.status)!="undefined"){
+                    var klipperstatus = result.status;
+                    var currentProgress = 0
+                    if(typeof(klipperstatus.virtual_sdcard)!="undefined"){
+                        if (klipperstatus.virtual_sdcard.file_position <= printstartbyte){
+                            currentProgress = 0
+                            variables.setProgress(currentProgress)
+                        }else if (klipperstatus.virtual_sdcard.file_position >= printendbyte){
+                            currentProgress = 100
+                            variables.setProgress(currentProgress)
                         }else{
-                            currentProgress = klipperstatus.virtual_sdcard.progress
+                            let currentPosition = klipperstatus.virtual_sdcard.file_position - printstartbyte;
+                            let maxPosition = printendbyte - printstartbyte;
+                            if(currentPosition > 0 && maxPosition > 0){
+                                currentProgress = (1/maxPosition*currentPosition)*100
+                            }else{
+                                currentProgress = klipperstatus.virtual_sdcard.progress
+                            }
+                            variables.setProgress(currentProgress)
                         }
-                        variables.setProgress(currentProgress)
-                    }
-                    if(variables.getStatus()=="printing"){
-                        connection.send('{"jsonrpc": "2.0", "method": "server.files.metadata", "params": {"filename": "'+variables.getCurrentFile()+'"}, "id": '+id+'}')
-                        if(currentProgress.toFixed(0)!=0&&currentProgress.toFixed(0)!=100){
-                            if(variables.getProgress()!=currentProgress.toFixed(0)){
-                                variables.setProgress(currentProgress)
-                                discordClient.user.setActivity("Printing: "+currentProgress.toFixed(0)+"%",{type: "WATCHING"})
-                                if(config.statusupdatepercent&&currentProgress.toFixed(2)!=0.00){
-                                    if(currentProgress % config.statusupdateinterval === 0){
-                                        variables.triggerStatusUpdate(discordClient)
+                        if(variables.getStatus()=="printing"){
+                            connection.send('{"jsonrpc": "2.0", "method": "server.files.metadata", "params": {"filename": "'+variables.getCurrentFile()+'"}, "id": '+id+'}')
+                            if(currentProgress.toFixed(0)!=0&&currentProgress.toFixed(0)!=100){
+                                if(variables.getProgress()!=currentProgress.toFixed(0)){
+                                    variables.setProgress(currentProgress)
+                                    discordClient.user.setActivity("Printing: "+currentProgress.toFixed(0)+"%",{type: "WATCHING"})
+                                    if(config.statusupdatepercent&&currentProgress.toFixed(2)!=0.00){
+                                        if(currentProgress % config.statusupdateinterval === 0){
+                                            variables.triggerStatusUpdate(discordClient)
+                                        }
                                     }
                                 }
                             }
