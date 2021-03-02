d94a3c57b91fba450e28bb0186b919351d4d8e3a
eliteSCHW31N <39838619+eliteSchwein@users.noreply.github.com>
2021-03-02 10:15:46 +0100

    update websocketevents handler

--- websocketevents.js
+++ websocketevents.js
@@ -4,6 +4,15 @@ const variables = require("./utils/variablesUtil")
 var getModule = (function(client,discordClient){
     client.on('connect', function(connection) {
         let id = Math.floor(Math.random() * 10000) + 1
+        connection.send('{"jsonrpc": "2.0", "method": "machine.update.status", "params":{"refresh": "true"}, "id": '+id+'}')
+        connection.send('{"jsonrpc": "2.0", "method": "printer.info", "id": '+id+'}')
+        connection.send('{"jsonrpc": "2.0", "method": "server.info", "id": '+id+'}')
+        connection.send('{"jsonrpc": "2.0", "method": "server.files.metadata", "params": {"filename": "'+variables.getCurrentFile()+'"}, "id": '+id+'}')
+        setInterval(function(){
+            connection.send('{"jsonrpc": "2.0", "method": "server.temperature_store", "id": '+id+'}')
+            connection.send('{"jsonrpc": "2.0", "method": "printer.objects.query", "params": {"objects": {"webhooks": null, "virtual_sdcard": null, "print_stats": null}}, "id": '+id+'}')
+            connection.send('{"jsonrpc": "2.0", "method": "machine.update.status", "params":{"refresh": "true"}, "id": '+id+'}')
+        },1000)
         fs.readdir(__dirname+"/websocket-events", (err, files) => {
             files.forEach(file => {
                 delete require.cache[require.resolve("./websocket-events/"+file)]
@@ -11,17 +20,6 @@ var getModule = (function(client,discordClient){
                 event(connection,discordClient)
             });
         });
-        setTimeout(function(){
-            setInterval(function(){
-                connection.send('{"jsonrpc": "2.0", "method": "server.temperature_store", "id": '+id+'}')
-                connection.send('{"jsonrpc": "2.0", "method": "printer.objects.query", "params": {"objects": {"webhooks": null, "virtual_sdcard": null, "print_stats": null}}, "id": '+id+'}')
-                connection.send('{"jsonrpc": "2.0", "method": "machine.update.status", "params":{"refresh": "true"}, "id": '+id+'}')
-            },1000)
-            connection.send('{"jsonrpc": "2.0", "method": "machine.update.status", "params":{"refresh": "true"}, "id": '+id+'}')
-            connection.send('{"jsonrpc": "2.0", "method": "printer.info", "id": '+id+'}')
-            connection.send('{"jsonrpc": "2.0", "method": "server.info", "id": '+id+'}')
-            connection.send('{"jsonrpc": "2.0", "method": "server.files.metadata", "params": {"filename": "'+variables.getCurrentFile()+'"}, "id": '+id+'}')
-        },250)
     });
     
 })
