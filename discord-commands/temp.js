701bb09f78d9df85bda81d278e4329664b9ca5a8
eliteSCHW31N <39838619+eliteSchwein@users.noreply.github.com>
2021-03-04 15:29:51 +0100

    update naming

--- discord-commands/temp.js
+++ discord-commands/temp.js
@@ -4,18 +4,18 @@ const master = false
 const discordDatabase = require('../discorddatabase')
 var variables = require("../utils/variablesUtil")
 var executeCommand = (function(command,channel,user,guild,discordClient,websocketConnection){
-    var debug = ""
+    var alltemps = ""
     var temps = variables.getTemps()
     for(var temp in temps){
         if(temp.includes("temperature_sensor")){
-            debug=debug.concat("**🌡"+temp.replace("temperature_sensor ","")+":**\n`"+temps[temp].temperatures[temps[temp].temperatures.length-1]+"°C`\n\n")
+            alltemps=alltemps.concat("**🌡"+temp.replace("temperature_sensor ","")+":**\n`"+temps[temp].temperatures[temps[temp].temperatures.length-1]+"°C`\n\n")
         }else if(temp.includes("extruder")||temp.includes("heater_bed")||temp.includes("heater_generic")){
-            debug=debug.concat("**♨"+temp.replace("heater_generic ","")+":**\n`Current:"+temps[temp].temperatures[temps[temp].temperatures.length-1]+"°C` `Target:"+temps[temp].targets[temps[temp].targets.length-1]+"°C` `Power:"+calculatePercent(temps[temp].powers[temps[temp].powers.length-1])+"%`\n\n")
+            alltemps=alltemps.concat("**♨"+temp.replace("heater_generic ","")+":**\n`Current:"+temps[temp].temperatures[temps[temp].temperatures.length-1]+"°C` `Target:"+temps[temp].targets[temps[temp].targets.length-1]+"°C` `Power:"+calculatePercent(temps[temp].powers[temps[temp].powers.length-1])+"%`\n\n")
         }else if(temp.includes("temperature_fan")){
-            debug=debug.concat("**❄"+temp+"**:\n`Current:"+temps[temp].temperatures[temps[temp].temperatures.length-1]+"°C` `Target:"+temps[temp].targets[temps[temp].targets.length-1]+"°C` `Speed:"+calculatePercent(temps[temp].speeds[temps[temp].speeds.length-1])+"`\n\n")
+            alltemps=alltemps.concat("**❄"+temp+"**:\n`Current:"+temps[temp].temperatures[temps[temp].temperatures.length-1]+"°C` `Target:"+temps[temp].targets[temps[temp].targets.length-1]+"°C` `Speed:"+calculatePercent(temps[temp].speeds[temps[temp].speeds.length-1])+"`\n\n")
         }
     }
-    channel.send(debug)
+    channel.send(alltemps)
 })
 function calculatePercent(input){
     var percent = 100*input
