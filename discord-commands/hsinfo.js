1ccd0385e4020ad77c9499ead70fe3cb8a0ccffa
eliteSCHW31N <39838619+eliteSchwein@users.noreply.github.com>
2021-03-04 16:05:27 +0100

    add experimentell disk feedback

--- discord-commands/hsinfo.js
+++ discord-commands/hsinfo.js
@@ -17,7 +17,25 @@ var executeCommand = (async function(command,channel,user,guild,discordClient,we
     var ramfeedback = "**📟 RAM:**\n"+
     "`Total: "+(ram.total/(Math.pow(1024,3))).toFixed(2)+"GB `\n"+
     "`Used: "+(ram.used/(Math.pow(1024,3))).toFixed(2)+"GB `\n"
-    channel.send(cpufeedback+"\n\n"+ramfeedback)
+    var disksfeedback = ""
+    for(var disk in disks){
+        disksfeedback=disksfeedback.concat("**💾 DISK **("+disk.device+")\n")
+        disksfeedback=disksfeedback.concat("`Type: "+disk.type+"`\n")
+        disksfeedback=disksfeedback.concat("`Modell: "+disk.name+"`\n")
+        disksfeedback=disksfeedback.concat("`Vendor: "+disk.vendor+"`\n")
+        disksfeedback=disksfeedback.concat("`Size: "+(disk.size/(Math.pow(1024,3)))+"`\n")
+        var partitionslist = ""
+        var usage = 0
+        for(var partition in partitions){
+            if(partition.name.startsWith(disk.device)){
+                partitionslist=partitionslist.concat(partition.mount+" ")
+                usage=usage+partition.used
+            }
+        }
+        disksfeedback=disksfeedback.concat("`Used: "+(usage/(Math.pow(1024,3)))+"`\n")
+        disksfeedback=disksfeedback.concat("`Parititions: "+partitionslist+"`\n\n")
+    }
+    channel.send(cpufeedback+"\n\n"+ramfeedback+"\n\n"+disksfeedback)
 })
 module.exports = executeCommand;
 module.exports.needAdmin = function(){return admin}
