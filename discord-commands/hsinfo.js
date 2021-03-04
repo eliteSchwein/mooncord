0833dd60bb7e16cc93559b1e09fafeb1d98595f2
eliteSCHW31N <39838619+eliteSchwein@users.noreply.github.com>
2021-03-04 16:28:32 +0100

    fix check

--- discord-commands/hsinfo.js
+++ discord-commands/hsinfo.js
@@ -32,10 +32,7 @@ var executeCommand = (async function(command,channel,user,guild,discordClient,we
             var usage = 0
             for(var partitionindex in partitions){
                 var partition = partitions[partitionindex]
-                console.log(String(partition.fs).startsWith("/dev/root"))
-                console.log(os.distro.includes("Raspbian"))
-                console.log(String(disk.device)=="mmcblk0")
-                if(String(partition.fs).startsWith(disk.device)||disk.device=="mmcblk0"&&os.distro.includes("Raspbian")&&String(partition.fs).startsWith("/dev/root")){
+                if(String(partition.fs).startsWith(disk.device)||String(disk.device)=="/dev/mmcblk0"&&os.distro.includes("Raspbian")&&String(partition.fs).startsWith("/dev/root")){
                     partitionslist=partitionslist.concat(partition.mount+" ")
                     usage=usage+partition.used
                 }
