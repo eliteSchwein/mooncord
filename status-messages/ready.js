27c8646054fb1ed0fc23a37afa419e5d67ee7142
eliteSCHW31N <39838619+eliteSchwein@users.noreply.github.com>
2021-03-09 08:20:31 +0100

    comment temporarly out

--- status-messages/ready.js
+++ status-messages/ready.js
@@ -7,7 +7,7 @@ const webcamUtil = require('../utils/webcamUtil')
 
 const getModule = async function (discordClient, channel, guild, user) {
   const database = discordDatabase.getDatabase()
-  discordClient.user.setActivity('for GCODE File...', { type: 'LISTENING' })
+  //discordClient.user.setActivity('for GCODE File...', { type: 'LISTENING' })
 
   if (typeof channel === 'undefined') {
     for (const guildid in database) {
