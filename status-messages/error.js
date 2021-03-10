6bc77e95d0bc7b24acac2370356fa8b40e5550ba
eliteSCHW31N <39838619+eliteSchwein@users.noreply.github.com>
2021-03-10 16:21:24 +0100

    fix error status message

--- status-messages/error.js
+++ status-messages/error.js
@@ -2,52 +2,14 @@ const statusUtil = require('../utils/statusUtil')
 const webcamUtil = require('../utils/webcamUtil')
 
 const getModule = async function (discordClient, channel, guild, user) {
-  const database = discordDatabase.getDatabase()
+  const snapshot = await webcamUtil.retrieveWebcam()
   discordClient.user.setActivity('wait for User', { type: 'LISTENING' })
 
-  const statusEmbed = statusUtil.getDefaultEmbed(user,'Printer Ready','#0099ff')
+  const statusEmbed = statusUtil.getDefaultEmbed(user,'A Error occured!','#c90000')
   statusEmbed
-    .addField('Mooncord Version', pjson.version, true)
-    .addField('Moonraker Version', moonrakerver, true)
-    .addField('Klipper Version', klipperver, true)
     .attachFiles(snapshot)
     .setImage(`attachment://${  snapshot.name}`)
 
   statusUtil.postStatus(discordClient,statusEmbed,channel)
-
-  if (typeof channel === 'undefined') {
-    for (const guildid in database) {
-      discordClient.guilds.fetch(guildid)
-        .then(async (guild) => {
-          const guilddatabase = database[guild.id]
-          const broadcastchannels = guilddatabase.statuschannels
-          for (const index in broadcastchannels) {
-            const channel = guild.channels.cache.get(broadcastchannels[index])
-            await sendMessage(channel, user)
-          }
-        })
-        .catch(console.error)
-    }
-  } else {
-    await sendMessage(channel, user)
-  }
-}
-
-async function sendMessage (channel, user) {
-  const snapshot = await webcamUtil.retrieveWebcam()
-  const statusEmbed = new Discord.MessageEmbed()
-    .setColor('#c90000')
-    .setTitle('A Error occured!')
-    .attachFiles(snapshot)
-    .setImage(`attachment://${  snapshot.name}`)
-    .setTimestamp()
-
-  if (typeof (user) === 'undefined') {
-    statusEmbed.setFooter('Automatic')
-  } else {
-    statusEmbed.setFooter(user.tag, user.avatarURL())
-  }
-
-  channel.send(statusEmbed)
 }
 module.exports = getModule
