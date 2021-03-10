488551698e562aa40390792c2868ea5d59d0b72a
eliteSCHW31N <39838619+eliteSchwein@users.noreply.github.com>
2021-03-10 13:31:23 +0100

    update command handler system

--- discord-commands/index.js
+++ discord-commands/index.js
@@ -1,72 +1,21 @@
-const fs = require('fs')
-const path = require('path')
-
-const config = require('../config.json')
-const discordDatabase = require('../discorddatabase')
-
-const executeCommands = function (command, channel, user, guild, discordClient, websocketConnection) {
-  if (command.toLowerCase().startsWith('index')) {
-    channel.send(`<@${  user.id  }> Ha you are so Funny (not)!`)
-    return
-  }
-  try {
-    if (!fs.existsSync(path.resolve(__dirname, `${command.toLowerCase().split(' ')[0]  }.js`))) {
-      channel.send(`<@${  user.id  }> The following Command couldn´t be found! \n> ${  config.prefix  }${command.split(' ')[0]  }\n use ${  config.prefix  }help`)
-      return
-    }
-  } catch (error) {
-    console.error(error)
-  }
-  delete require.cache[require.resolve(`./${  command.split(' ')[0]}`)]
-  const commandModule = require(`./${  command.split(' ')[0]}`)
-  if (commandModule.needMaster() && user.id !== config.masterid) {
-      channel.send(`<@${  user.id  }> You are not allowed to execute the following Command! \n> ${  config.prefix  }${command.split(' ')[0]}`)
-      return
-    }
-  if (commandModule.needAdmin() && !isAdmin(user, guild)) {
-      channel.send(`<@${  user.id  }> You are not allowed to execute the following Command! \n> ${  config.prefix  }${command.split(' ')[0]}`)
-      return
-    }
-  if (!isAllowed(user, guild)) {
-    channel.send(`<@${  user.id  }> You are not allowed to execute the following Command! \n> ${  config.prefix  }${command.split(' ')[0]}`)
-    return
-  }
-  commandModule(command, channel, user, guild, discordClient, websocketConnection)
-}
-module.exports = executeCommands
-
-function isAdmin (user, guild) {
-  const database = discordDatabase.getGuildDatabase(guild)
-  const member = guild.member(user)
-  if (user.id === config.masterid) {
-    return true
-  }
-  if (database.adminusers.includes(user.id)) {
-    return true
-  }
-  for (const memberole in member.roles.cache) {
-    if (database.adminroles.includes(memberole)) {
-      return true
-    }
-  }
-  return false
-}
-function isAllowed (user, guild) {
-  const database = discordDatabase.getGuildDatabase(guild)
-  const member = guild.member(user)
-  if (database.accesseveryone === true) {
-    return true
-  }
-  if (isAdmin(user, guild)) {
-    return true
-  }
-  if (database.accessusers.includes(user.id)) {
-    return true
-  }
-  for (const memberole in member.roles.cache) {
-    if (database.accessroles.includes(memberole)) {
-      return true
-    }
-  }
-  return false
+module.exports = {
+  addaccess: require("./addaccess"),
+  addadmin: require("./addadmin"),
+  addchannel: require("./addchannel"),
+  execute: require("./execute"),
+  generalaccess: require("./generalaccess"),
+  help: require("./help"),
+  info: require("./info"),
+  listprints: require("./listprints"),
+  pause: require("./pause"),
+  print: require("./print"),
+  reboot: require("./reboot"),
+  removeaccess: require("./removeaccess"),
+  removeadmin: require("./removeadmin"),
+  removechannel: require("./removechannel"),
+  resume: require("./resume"),
+  shutdown: require("./shutdown"),
+  status: require("./status"),
+  stop: require("./stop"),
+  temp: require("./temp"),
 }
--- discord-events/command.js
+++ discord-events/command.js
@@ -1,5 +1,9 @@
+const fs = require('fs')
+const path = require('path')
+
 const config = require('../config.json')
-const commandHandler = require('../discord-commands')
+const discordDatabase = require('../discorddatabase')
+const commands = require('../discord-commands')
 
 const enableEvent = function (discordClient, websocketConnection) {
   discordClient.on('message', msg => {
@@ -10,9 +14,72 @@ const enableEvent = function (discordClient, websocketConnection) {
     if (msg.channel.type !== 'text') {
       return
     }
+    const command = msg.toString().slice(config.prefix.length)
+    if(command.startsWith('index')){
+      channel.send(`<@${  user.id  }> The following Command couldn´t be found! \n> ${  config.prefix  }${command.split(' ')[0]  }\n use ${  config.prefix  }help`)
+      return
+    }
+    try {
+      if (!fs.existsSync(path.resolve(__dirname, `../discord-commands/${command.toLowerCase().split(' ')[0]  }.js`))) {
+        channel.send(`<@${  user.id  }> The following Command couldn´t be found! \n> ${  config.prefix  }${command.split(' ')[0]  }\n use ${  config.prefix  }help`)
+        return
+      }
+    } catch (error) {
+      console.error(error)
+    }
+    const commandModule = commands[command.split(' ')[0]]
+    if (commandModule.needMaster() && user.id !== config.masterid) {
+      channel.send(`<@${  user.id  }> You are not allowed to execute the following Command! \n> ${  config.prefix  }${command.split(' ')[0]}`)
+      return
+    }
+    if (commandModule.needAdmin() && !isAdmin(user, guild)) {
+        channel.send(`<@${  user.id  }> You are not allowed to execute the following Command! \n> ${  config.prefix  }${command.split(' ')[0]}`)
+        return
+      }
+    if (!isAllowed(user, guild)) {
+      channel.send(`<@${  user.id  }> You are not allowed to execute the following Command! \n> ${  config.prefix  }${command.split(' ')[0]}`)
+      return
+    }
     if (msg.toString().startsWith(config.prefix)) {
       commandHandler(msg.toString().slice(config.prefix.length), msg.channel, msg.author, msg.channel.guild, discordClient, websocketConnection)
     }
+    commandModule(command, channel, user, guild, discordClient, websocketConnection)
   })
 }
 module.exports = enableEvent
+
+function isAdmin (user, guild) {
+  const database = discordDatabase.getGuildDatabase(guild)
+  const member = guild.member(user)
+  if (user.id === config.masterid) {
+    return true
+  }
+  if (database.adminusers.includes(user.id)) {
+    return true
+  }
+  for (const memberole in member.roles.cache) {
+    if (database.adminroles.includes(memberole)) {
+      return true
+    }
+  }
+  return false
+}
+function isAllowed (user, guild) {
+  const database = discordDatabase.getGuildDatabase(guild)
+  const member = guild.member(user)
+  if (database.accesseveryone === true) {
+    return true
+  }
+  if (isAdmin(user, guild)) {
+    return true
+  }
+  if (database.accessusers.includes(user.id)) {
+    return true
+  }
+  for (const memberole in member.roles.cache) {
+    if (database.accessroles.includes(memberole)) {
+      return true
+    }
+  }
+  return false
+}
