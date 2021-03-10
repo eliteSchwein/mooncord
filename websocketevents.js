35846821152f4ae5c3c85567b23e5b345977edd8
eliteSCHW31N <39838619+eliteSchwein@users.noreply.github.com>
2021-03-10 08:41:11 +0100

    update debug

--- websocketevents.js
+++ websocketevents.js
@@ -17,7 +17,7 @@ const getModule = async function (client, discordClient) {
       connection.on('message', (message) => {
         files.forEach(file => {
           const event = events[file.replace('.js','')]
-          console.log(event)
+          console.log(file+' '+event)
           event(message, connection, discordClient)
         })
       })
