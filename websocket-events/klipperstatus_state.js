5d04cfbc2e25db619ab4bcaa08392356dfb3989e
eliteSCHW31N <39838619+eliteSchwein@users.noreply.github.com>
2021-03-11 20:42:34 +0100

    add debug

--- websocket-events/klipperstatus_state.js
+++ websocket-events/klipperstatus_state.js
@@ -6,6 +6,7 @@ const event = (message, connection, discordClient) => {
     if (message.type === 'utf8') {
       const messageJson = JSON.parse(message.utf8Data)
       const {result} = messageJson
+      console.log(result)
       if (typeof (result) !== 'undefined' && typeof (result.status) !== 'undefined') {
           const klipperstatus = result.status
           if (typeof (klipperstatus.print_stats) !== 'undefined') {
