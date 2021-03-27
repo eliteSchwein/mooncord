c8c8b76bae340c5d029e774acf2ed082f935464d
eliteSCHW31N <business@eliteschw31n.de>
2021-03-27 22:50:09 +0100

    remove shutdown and reboot command due security reasons

--- discord-commands/reboot.js
+++
@@ -1,8 +0,0 @@
-const admin = true
-const master = false
-const executeCommand = function (command, channel, user, guild, discordClient, websocketConnection) {
-  channel.send('Working Soon!')
-}
-module.exports = executeCommand
-module.exports.needAdmin = function () { return admin }
-module.exports.needMaster = function () { return master }
