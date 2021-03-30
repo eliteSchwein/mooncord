d0768d75e12352d2c5ccd17d4c72ee794ad9ce36
eliteSCHW31N <business@eliteschw31n.de>
2021-03-30 20:48:47 +0200

    update template command

--- slash-commands/test-1.js
+++ slash-commands/test-1.js
@@ -3,6 +3,7 @@ const { SlashCommand } = require('slash-create');
 module.exports = class HelloCommand extends SlashCommand {
     constructor(creator) {
         super(creator, {
+            guildIDs: '626717239210672139',
             name: 'test-1',
             description: 'Says hello to you.'
         });
