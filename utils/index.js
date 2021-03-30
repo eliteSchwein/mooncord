1aada734e4449098e5382e2ec3ee1e52c180ec5d
eliteSCHW31N <business@eliteschw31n.de>
2021-03-30 22:46:54 +0200

    add utils index

---
+++ utils/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+    system: require('./hsUtil'),
+    permission: require('./permissionUtil'),
+    status: require('./statusUtil'),
+    thumbnail: require('./thumbnailUtil'),
+    variables: require('./variablesUtil'),
+    webcam: require('./webcamUtil')
+}
