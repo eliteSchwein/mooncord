a3c1f94eeaac7efc17c6821e7b5c844373e7ee72
eliteSCHW31N <business@eliteschw31n.de>
2021-03-31 20:41:03 +0200

    remove index

--- utils/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    system: require('./hsUtil'),
-    permission: require('./permissionUtil'),
-    status: require('./statusUtil'),
-    thumbnail: require('./thumbnailUtil'),
-    variables: require('./variablesUtil'),
-    webcam: require('./webcamUtil'),
-    database: require('./databaseUtil')
-}
