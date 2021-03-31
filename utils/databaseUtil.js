d71c68fda817648f472cc03db18fe65d8993dbed
eliteSCHW31N <business@eliteschw31n.de>
2021-03-31 15:39:40 +0200

    update database paths

--- utils/databaseUtil.js
+++ utils/databaseUtil.js
@@ -5,26 +5,23 @@ module.exports = {}
 
 module.exports.getGuildDatabase = function (guild) {
   const database = require(databasepath)
-  if (typeof database[guild.id] === 'undefined') {
+  if (typeof database.guild[guild.id] === 'undefined') {
     console.log(`No Database for ${guild.name} found!\nGenerate base config!`)
-    database[guild.id] = {
-      statuschannels: [],
+    database.guilds[guild.id] = {
+      broadcastchannels: [],
       adminusers: [],
-      adminroles: [],
-      accessrole: [],
-      accessusers: [],
-      accesseveryone: false
+      adminroles: []
     }
-    this.updateDatabase(database[guild.id], guild)
+    this.updateDatabase(database.guild[guild.id], guild)
   }
-  return database[guild.id]
+  return database.guild[guild.id]
 }
 module.exports.getDatabase = function () {
   return require(databasepath)
 }
 module.exports.updateDatabase = function (data, guild) {
   const database = require(databasepath)
-  database[guild.id] = data
+  database.guild[guild.id] = data
   fs.writeFile(databasepath, JSON.stringify(database), (err) => {
     if (err) { throw err }
     console.log('The Database has been saved!')
