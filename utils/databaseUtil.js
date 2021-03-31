71aaf26864907d7abfb91531dcac14809c432d5a
eliteSCHW31N <business@eliteschw31n.de>
2021-03-31 15:41:15 +0200

    fix database util, update default database

--- utils/databaseUtil.js
+++ utils/databaseUtil.js
@@ -5,7 +5,7 @@ module.exports = {}
 
 module.exports.getGuildDatabase = function (guild) {
   const database = require(databasepath)
-  if (typeof database.guild[guild.id] === 'undefined') {
+  if (typeof database.guilds[guild.id] === 'undefined') {
     console.log(`No Database for ${guild.name} found!\nGenerate base config!`)
     database.guilds[guild.id] = {
       broadcastchannels: [],
@@ -14,14 +14,14 @@ module.exports.getGuildDatabase = function (guild) {
     }
     this.updateDatabase(database.guild[guild.id], guild)
   }
-  return database.guild[guild.id]
+  return database.guilds[guild.id]
 }
 module.exports.getDatabase = function () {
   return require(databasepath)
 }
 module.exports.updateDatabase = function (data, guild) {
   const database = require(databasepath)
-  database.guild[guild.id] = data
+  database.guilds[guild.id] = data
   fs.writeFile(databasepath, JSON.stringify(database), (err) => {
     if (err) { throw err }
     console.log('The Database has been saved!')
