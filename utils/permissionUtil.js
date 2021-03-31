03e7cd5ed9a8fbedfb7d6814a897fc56dc4130cf
eliteSCHW31N <business@eliteschw31n.de>
2021-03-31 18:54:55 +0200

    fix permission util

--- utils/permissionUtil.js
+++ utils/permissionUtil.js
@@ -1,19 +1,19 @@
 const config = require('../config.json')
-const discordDatabase = require('../discorddatabase')
-const core = require('../mooncord')
+const database = require('./databaseUtil')
+const { discordClient } = require('../clients')
 
 module.exports.hasAdmin = async function (user, guildid) {
   if (user.id === config.masterid) {
     return true
   }
-  const guild = await core.getDiscordClient().guilds.fetch(guildid)
-  const database = discordDatabase.getGuildDatabase(guild)
-  if (database.adminusers.includes(user.id)) {
+  const guild = await discordClient.getClient().getDiscordClient().guilds.fetch(guildid)
+  const guilddatabase = database.getGuildDatabase(guild)
+  if (guilddatabase.adminusers.includes(user.id)) {
     return true
   }
   const member = await guild.members.fetch(user.id)
   for (const memberole in member.roles.cache) {
-    if (database.adminroles.includes(memberole)) {
+    if (guilddatabase.adminroles.includes(memberole)) {
       return true
     }
   }
