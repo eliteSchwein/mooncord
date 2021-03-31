a94cba542b0d8fa6a33f16964e54f05f64b1ec3b
eliteSCHW31N <business@eliteschw31n.de>
2021-03-31 21:18:38 +0200

    add try catch

--- slash-commands/channel.js
+++ slash-commands/channel.js
@@ -19,29 +19,35 @@ module.exports = class HelloCommand extends SlashCommand {
     }
 
     async run(ctx) {
-        if (!await permission.hasAdmin(ctx.user, ctx.guildID)) {
-            return `You dont have the Permissions, ${ctx.user.username}!`
+        try {
+            if (!await permission.hasAdmin(ctx.user, ctx.guildID)) {
+                return `You dont have the Permissions, ${ctx.user.username}!`
+            }
+
+            let channel
+            let channelresult
+
+            if (typeof (ctx.options.channel) === 'undefined') {
+                channelresult = await editChannel(ctx.channelID, ctx.guildID)
+                channel = 'This Channel'
+            } else {
+                channelresult = await editChannel(ctx.options.channel, ctx.guildID)
+                channel = `<#${ctx.options.channel}>`
+            }
+
+            if (typeof (channelresult) === 'undefined') {
+                return `${channel} is not a Text Channel, ${ctx.user.username}!`
+            }
+
+            if (channelresult) {
+                return `${channel} is now a Broadcast Channel, ${ctx.user.username}!`
+            } else {
+                return `${channel} is not longer a Broadcast Channel, ${ctx.user.username}!`
+            }
         }
-
-        let channel
-        let channelresult
-
-        if (typeof (ctx.options.channel) === 'undefined') {
-            channelresult = await editChannel(ctx.channelID, ctx.guildID)
-            channel = 'This Channel'
-        } else {
-            channelresult = await editChannel(ctx.options.channel, ctx.guildID)
-            channel = `<#${ctx.options.channel}>`
-        }
-
-        if (typeof (channelresult) === 'undefined') {
-            return `${channel} is not a Text Channel, ${ctx.user.username}!`
-        }
-
-        if (channelresult) {
-            return `${channel} is now a Broadcast Channel, ${ctx.user.username}!`
-        } else {
-            return `${channel} is not longer a Broadcast Channel, ${ctx.user.username}!`
+        catch (err) {
+            console.log(err)
+            return "An Error occured!";
         }
     }
 }
@@ -53,16 +59,16 @@ async function editChannel(channelid, guildid) {
     if (channel.type !== 'text') {
         return undefined
     }
-    if (guilddatabase.statuschannels.includes(channelid)) {
-        const index = guilddatabase.statuschannels.indexOf(channelid)
+    if (guilddatabase.broadcastchannels.includes(channelid)) {
+        const index = guilddatabase.broadcastchannels.indexOf(channelid)
         if (index > -1) {
-            guilddatabase.statuschannels.splice(index, 1)
+            guilddatabase.broadcastchannels.splice(index, 1)
         }
         database.updateDatabase(guilddatabase, guild)
         return false
     }
 
-    guilddatabase.statuschannels.push(channelid)
+    guilddatabase.broadcastchannels.push(channelid)
     database.updateDatabase(guilddatabase, guild)
 
     return true
