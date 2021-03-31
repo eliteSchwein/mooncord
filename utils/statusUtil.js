9922a2cbb8e5c8f67465bcf5fefdb991ae1c202e
eliteSCHW31N <business@eliteschw31n.de>
2021-03-31 19:51:48 +0200

    update debug

--- utils/statusUtil.js
+++ utils/statusUtil.js
@@ -18,7 +18,7 @@ module.exports.triggerStatusUpdate = async function (channel, user) {
 }
 
 module.exports.getManualStatusEmbed = async function (user) {
-  console.log(utils.variables)
+    console.log(utils)
     const statusEvent = status[utils.variables.getStatus()]
     const embed = statusEvent(user)
     return embed
