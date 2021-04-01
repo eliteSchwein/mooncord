9c51985c18f085032d8a7545b4ce34e28a34a0d9
eliteSCHW31N <business@eliteschw31n.de>
2021-04-01 22:28:10 +0200

    add admin edit function

--- discord-commands/admin.js
+++ discord-commands/admin.js
@@ -1,4 +1,5 @@
 const { SlashCommand, CommandOptionType } = require('slash-create');
+const permission = require('../utils/permissionUtil')
 
 module.exports = class HelloCommand extends SlashCommand {
     constructor(creator) {
@@ -33,13 +34,63 @@ module.exports = class HelloCommand extends SlashCommand {
 
     async run(ctx) {
         try {
-            
+            if (!permission.isMaster(user)) {
+                return `You dont have the Permissions, ${ctx.user.username}!`
+            }
+
+            let isRole
+            let adminid
+
+            if (ctx.subcommands[0] === 'role') {
+                isRole = true
+                adminid = ctx.options.role.role
+            }
+
+            if (ctx.subcommands[0] === 'user') {
+                isRole = false
+                adminid = ctx.options.user.user
+            }
+
+            const result = editAdmin(isRole, adminid, ctx.guildID)
+
+            let answermention = `<@${adminid}>`
+
+            if (!isRole) {
+                answermention = answermention.replace(/<@/g,'<')
+            }
+
+            if (result) {
+                return `${answermention} is now a Admin, ${ctx.user.username}!`
+            } else {
+                return `${answermention} is not longer a Admin, ${ctx.user.username}!`
+            }
         }
         catch (err) {
             console.log(err)
             return "An Error occured!";
         }
-        console.log(ctx)
-        return `Hello, ${ctx.user.username}!`;
     }
+}
+
+async function editAdmin(isRole, adminid, guildid) {
+    const guild = await discordClient.getClient().guilds.fetch(guildid)
+    const guilddatabase = database.getGuildDatabase(guild)
+    let adminarray = 'adminusers'
+
+    if (isRole) {
+        adminarray = 'adminroles'
+    }
+    if (guilddatabase[adminarray].includes(adminid)) {
+        const index = guilddatabase[adminarray].indexOf(adminid)
+        if (index > -1) {
+            guilddatabase[adminarray].splice(index, 1)
+        }
+        database.updateDatabase(guilddatabase, guild)
+        return false
+    }
+
+    guilddatabase[adminarray].push(adminid)
+    database.updateDatabase(guilddatabase, guild)
+
+    return true
 }
