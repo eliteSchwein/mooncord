9c148a9a5376b7da8c5830762107178012a6050a
eliteSCHW31N <business@eliteschw31n.de>
2021-04-01 17:19:44 +0200

    add debug

--- utils/statusUtil.js
+++ utils/statusUtil.js
@@ -81,6 +81,7 @@ function postStatus(message) {
       .then(async (guild) => {
         const guilddatabase = botdatabase.guilds[guild.id]
         for (const index in guilddatabase.broadcastchannels) {
+          console.log(guilddatabase.broadcastchannels[index])
           const channel = guild.channels.cache.get(guilddatabase.broadcastchannels[index])
           channel.send(message)
         }
