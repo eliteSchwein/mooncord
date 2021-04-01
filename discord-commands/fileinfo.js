3ea7fe4f51752f94be952343a15f4abba6bf43d0
eliteSCHW31N <business@eliteschw31n.de>
2021-04-01 20:22:21 +0200

    update debug

--- discord-commands/fileinfo.js
+++ discord-commands/fileinfo.js
@@ -54,7 +54,7 @@ module.exports = class HelloCommand extends SlashCommand {
                     } else {
                         let files = {}
                         if (typeof (commandFeedback.files) !== 'undefined') {
-                            console.log(commandFeedback.files[0].name)
+                            console.log(commandFeedback.files[0])
                             files = {
                                 name: commandFeedback.files[0].name,
                                 file: commandFeedback.files[0].attachment
