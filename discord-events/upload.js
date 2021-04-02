a93fb15a4934817369f869764b5c9c916cf5988f
eliteSCHW31N <business@eliteschw31n.de>
2021-04-02 23:12:18 +0200

    add aync

--- discord-events/upload.js
+++ discord-events/upload.js
@@ -23,8 +23,7 @@ const enableEvent = function (discordClient) {
     if (!msg.attachments.array()[0].name.endsWith('.gcode')) {
       return
     }
-    console.log(permission.hasAdmin(msg.author, msg.guild.id))
-    if (!permission.hasAdmin(msg.author, msg.guild.id)) {
+    if (!await permission.hasAdmin(msg.author, msg.guild.id)) {
       return
     }
     const guilddatabase = database.getGuildDatabase(msg.guild)
