edda67f5c5e7710f6b7afd0d7718dfa1345595df
eliteSCHW31N <business@eliteschw31n.de>
2021-04-02 17:01:15 +0200

    add temps command

---
+++ discord-commands/temp.js
@@ -0,0 +1,34 @@
+const { SlashCommand } = require('slash-create');
+const variables = require('../utils/variablesUtil')
+
+module.exports = class HelloCommand extends SlashCommand {
+    constructor(creator) {
+        super(creator, {
+            guildIDs: '626717239210672139',
+            name: 'temp',
+            description: 'Get the current Temperatures from Klipper.'
+        });
+        this.filePath = __filename;
+    }
+
+    async run(ctx) {
+        try {
+            let alltemps = ''
+            const temps = variables.getTemps()
+            for (const temp in temps) {
+                if (temp.includes('temperature_sensor')) {
+                    alltemps = alltemps.concat(`**🌡${temp.replace('temperature_sensor ', '')}:**\n\`${temps[temp].temperatures[temps[temp].temperatures.length - 1]}°C\`\n\n`)
+                } else if (temp.includes('extruder') || temp.includes('heater_bed') || temp.includes('heater_generic')) {
+                    alltemps = alltemps.concat(`**♨${temp.replace('heater_generic ', '')}:**\n\`Current:${temps[temp].temperatures[temps[temp].temperatures.length - 1]}°C\` \`Target:${temps[temp].targets[temps[temp].targets.length - 1]}°C\` \`Power:${calculatePercent(temps[temp].powers[temps[temp].powers.length - 1])}%\`\n\n`)
+                } else if (temp.includes('temperature_fan')) {
+                    alltemps = alltemps.concat(`**❄${temp}**:\n\`Current:${temps[temp].temperatures[temps[temp].temperatures.length - 1]}°C\` \`Target:${temps[temp].targets[temps[temp].targets.length - 1]}°C\` \`Speed:${calculatePercent(temps[temp].speeds[temps[temp].speeds.length - 1])}\`\n\n`)
+                }
+            }
+            return alltemps
+        }
+        catch (err) {
+            console.log(err)
+            return "An Error occured!"
+        }
+    }
+}
