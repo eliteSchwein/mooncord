1fd7b52a672f7cef5eff88171fd53e9ab1307d5e
eliteSCHW31N <business@eliteschw31n.de>
2021-04-03 00:54:14 +0200

    add tasked upload

--- discord-events/upload.js
+++ discord-events/upload.js
@@ -9,7 +9,10 @@ const config = require('../config.json')
 const database = require('../utils/databaseUtil')
 const permission = require('../utils/permissionUtil')
 
-const moonrakerurl = config.moonrakerurl
+const { waitUntil } = require('async-wait-until')
+
+let uploadList = []
+let uploadWaitTimer = 0
 
 const enableEvent = function (discordClient) {
   discordClient.on('message', async (msg) => {
@@ -34,42 +37,76 @@ const enableEvent = function (discordClient) {
       const channel = msg.guild.channels.cache.get(guilddatabase.broadcastchannels[index])
       if (channel.id === msg.channel.id) {
         const gcodefile = msg.attachments.array()[0]
-        const formData = new FormData()
-        const tempFile = fs.createWriteStream(`temp/${gcodefile.name.replace(' ', '_')}`)
-        tempFile.on('finish', () => {
-          console.log(logSymbols.info, `upload ${gcodefile.name.replace(' ', '_')}`.upload)
-          formData.append('file', fs.createReadStream(`temp/${gcodefile.name.replace(' ', '_')}`), gcodefile.name)
-          axios
-            .post(`${moonrakerurl}/server/files/upload`, formData, {
-              headers: formData.getHeaders()
-            })
-            .then(res => {
-              console.log(logSymbols.success, `uploaded ${gcodefile.name.replace(' ', '_')}`.uploadsuccess)
-              msg.react('✅')
-              fs.unlink(`temp/${gcodefile.name.replace(' ', '_')}`, (err) => {
-                if (err) {
-                  console.log((err).error)
-                }
-              })
-            })
-            .catch(err => {
-              if (err) {
-                console.log((err).error)
-                channel.send(`<@${config.masterid}> Please Check the Console!`)
-                console.log(logSymbols.error, 'Upload Failed! Check your config!'.error)
-                fs.unlink(`temp/${gcodefile.name.replace(' ', '_')}`, (err2) => {
-                  if (err2) {
-                    console.log((err2).error)
-                  }
-                })
-              }
-            })
-        })
-        https.get(gcodefile.url, (response) => {
-          response.pipe(tempFile)
-        })
+        const uploadData = {
+          'gcodefile': gcodefile,
+          'message': msg
+        }
+        uploadList.push(uploadData)
+        uploadWaitTimer = 5
+        if (uploadWaitTimer === 0) {
+          const timer = setInterval(async () => {
+            if (uploadWaitTimer === 0) {
+              upload()
+              clearInterval(timer)
+            }
+            uploadWaitTimer --
+          }, 1000)
+        }
       }
     }
   })
 }
 module.exports = enableEvent
+
+async function upload() {
+  if (uploadList.length === 0) {
+    return
+  }
+  if (uploadWaitTimer !== 0) {
+    return
+  }
+  await uploadFile(uploadList[0].gcodefile, uploadList[0].message)
+  uploadList = uploadList.splice(0, 1)
+  upload()
+}
+
+async function uploadFile(file, message) {
+  const formData = new FormData()
+  const tempFile = fs.createWriteStream(`temp/${file.name.replace(' ', '_')}`)
+  const done = false
+  tempFile.on('finish', () => {
+    console.log(logSymbols.info, `upload ${file.name.replace(' ', '_')}`.upload)
+    formData.append('file', fs.createReadStream(`temp/${file.name.replace(' ', '_')}`), file.name)
+    axios
+      .post(`${config.moonrakerurl}/server/files/upload`, formData, {
+        headers: formData.getHeaders()
+      })
+      .then(res => {
+        console.log(logSymbols.success, `uploaded ${file.name.replace(' ', '_')}`.uploadsuccess)
+        message.react('✅')
+        fs.unlink(`temp/${file.name.replace(' ', '_')}`, (err) => {
+          if (err) {
+            console.log((err).error)
+          }
+        })
+        done = true
+      })
+      .catch(err => {
+        if (err) {
+          console.log((err).error)
+          message.channel.send(`<@${config.masterid}> Please Check the Console!`)
+          console.log(logSymbols.error, 'Upload Failed! Check your config!'.error)
+          fs.unlink(`temp/${file.name.replace(' ', '_')}`, (err2) => {
+            if (err2) {
+              console.log((err2).error)
+            }
+          })
+          done = true
+        }
+      })
+  })
+  https.get(file.url, (response) => {
+    response.pipe(tempFile)
+  })
+  await waitUntil(() => done === true)
+}
