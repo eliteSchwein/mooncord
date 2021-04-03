b0e8eb9b5fe5cfd7ec92d2d39db527d1fe0b4057
eliteSCHW31N <business@eliteschw31n.de>
2021-04-03 15:53:32 +0200

    remove temp from index

--- websocket-events/index.js
+++ websocket-events/index.js
@@ -8,6 +8,5 @@ module.exports = {
   klipperstatus_state_update: require('./klipperstatus_state_update'),
   klipperstatus_state: require('./klipperstatus_state'),
   klipperstatus_vsdcard: require('./klipperstatus_vsdcard'),
-  temps: require('./temps'),
   versions: require('./versions')
 }
