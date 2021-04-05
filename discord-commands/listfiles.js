a9f9bfd112a67b349b3012f326f9b3c796f1fab2
eliteSCHW31N <business@eliteschw31n.de>
2021-04-06 00:21:46 +0200

    add debug

--- discord-commands/listfiles.js
+++ discord-commands/listfiles.js
@@ -50,10 +50,11 @@ module.exports = class HelloCommand extends SlashCommand {
                             name: thumbnail.name,
                             file: thumbnail.attachment
                         }
-                        const commandmessage = await ctx.send({
+                        const commandmessage = ''
+                        console.log(ctx.send({
                             file: files,
-                            embeds: [await commandFeedback.toJSON()]
-                        });
+                            embeds: [commandFeedback.toJSON()]
+                        }))
                         const channel = await discordClient.getClient().channels.fetch(ctx.channelID)
                         const message = await channel.messages.fetch(commandmessage.id)
                         message.react('◀️')
