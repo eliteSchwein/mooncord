f4f39ce45381e92a558eef3b0dd9161a24c0a40f
eliteSCHW31N <business@eliteschw31n.de>
2021-04-05 22:36:25 +0200

    add checks

--- discord-events/printjobstart.js
+++ discord-events/printjobstart.js
@@ -2,12 +2,12 @@ const chatUtil = require('../utils/chatUtil')
 
 const enableEvent = function (discordClient) {
     discordClient.on('messageReactionAdd', (msgreaction, user) => {
-        //if (!chatUtil.hasMessageEmbed(msg)) {
-        //    return
-        //}
-        //if (msg.author.id !== discordClient.user.id) {
-        //    return
-        //}
+        if (!chatUtil.hasMessageEmbed(msgreaction.Message)) {
+            return
+        }
+        if (user.id === discordClient.user.id) {
+            return
+        }
         //const title = msg.embeds[0].title
         console.log(msgreaction)
     })
