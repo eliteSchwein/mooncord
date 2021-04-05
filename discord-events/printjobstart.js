ca7c2d3dcbf1265ebd9e020ab326eff57ef2224b
eliteSCHW31N <business@eliteschw31n.de>
2021-04-05 22:33:25 +0200

    replace event

--- discord-events/printjobstart.js
+++ discord-events/printjobstart.js
@@ -1,15 +1,15 @@
 const chatUtil = require('../utils/chatUtil')
 
 const enableEvent = function (discordClient) {
-    discordClient.on('message', msg => {
-        if (!chatUtil.hasMessageEmbed(msg)) {
-            return
-        }
-        if (msg.author.id !== discordClient.user.id) {
-            return
-        }
-        const title = msg.embeds[0].title
-        console.log(title)
+    discordClient.on('messageReactionAdd', (msgreaction, user) => {
+        //if (!chatUtil.hasMessageEmbed(msg)) {
+        //    return
+        //}
+        //if (msg.author.id !== discordClient.user.id) {
+        //    return
+        //}
+        //const title = msg.embeds[0].title
+        console.log(msgreaction)
     })
 }
 module.exports = enableEvent
