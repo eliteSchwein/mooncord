360df2f9bf3147a861776a13f80aef3475e5a54a
eliteSCHW31N <business@eliteschw31n.de>
2021-04-05 23:05:17 +0200

    fix reaction handler

--- discord-events/printjobstart.js
+++ discord-events/printjobstart.js
@@ -21,14 +21,16 @@ const enableEvent = function (discordClient) {
         }
         messageReaction.remove()
         if (messageReaction.emoji === '❌') {
-            await message.edit(`Print Job request aborted, ${user.username}!`)
+            message.delete()
+            message.channel.send(`Print Job request aborted, ${user.username}!`)
             return
         }
         
         if (messageReaction.emoji === '✅') {
+            message.delete()
             const gcodefile = message.embeds[0].author.anme
             const id = Math.floor(Math.random() * 10000) + 1
-            await message.edit(`Print Job request executed, ${user.username}!`)
+            message.channel.send(`Print Job request executed, ${user.username}!`)
             moonrakerClient.getConnection().send(`{"jsonrpc": "2.0", "method": "printer.print.start", "params": {"filename": "${gcodefile}"}, "id": ${id}}`)
         }
     })
