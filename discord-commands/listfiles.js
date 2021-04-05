3f2f905f1fb62db1d5f6ed59cd5da1af0e8a1950
eliteSCHW31N <business@eliteschw31n.de>
2021-04-06 00:25:46 +0200

    temp removal

--- discord-commands/listfiles.js
+++ discord-commands/listfiles.js
@@ -51,7 +51,6 @@ module.exports = class HelloCommand extends SlashCommand {
                             file: thumbnail.attachment
                         }
                         const commandmessage = await ctx.send({
-                            file: files,
                             embeds: [commandFeedback.toJSON()]
                         });
                         const channel = await discordClient.getClient().channels.fetch(ctx.channelID)
