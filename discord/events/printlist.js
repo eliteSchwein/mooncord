6b0d50175affe2f9c9aedd4628556e78c5c3d650
eliteSCHW31N <business@eliteschw31n.de>
2021-04-08 20:45:44 +0200

    add debug

--- discord/events/printlist.js
+++ discord/events/printlist.js
@@ -67,6 +67,7 @@ async function executeMessage(message, user) {
 
 async function handler (message) {
     const messageJson = JSON.parse(message.utf8Data)
+    console.log(message.channel.id)
     if (JSON.stringify(messageJson).match(/(filename|modified)/g)) {
         commandFeedback[message.channel.id] = await chatUtil.generatePageEmbed(
             pageUp,
