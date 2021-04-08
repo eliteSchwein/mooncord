5692a9e09fc167e4b1922d1b8b46e5aaf1aa4b98
eliteSCHW31N <business@eliteschw31n.de>
2021-04-08 22:30:07 +0200

    update debug

--- websocket-events/klipperstates.js
+++ websocket-events/klipperstates.js
@@ -9,7 +9,7 @@ const event = (message, connection, discordClient) => {
     const { params } = messageJson
     if (typeof (methode) === 'undefined') { return }
     if (!Object.keys(states).includes(methode)) { return }
-    console.log(states[methode])
+    console.log(methode)
     if (typeof (states[methode].preventStatus) !== 'undefined') {
       console.log((states[methode].preventStatus.some(invalidState => variables.getStatus() === invalidState)))
       if (states[methode].preventStatus.some(invalidState => variables.getStatus() === invalidState)) { return }
