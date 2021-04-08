ab890af202bf27f5d2f5468cbf9a54b2b5a16a36
eliteSCHW31N <business@eliteschw31n.de>
2021-04-08 21:57:12 +0200

    reorder

--- websocket-events/index.js
+++ websocket-events/index.js
@@ -1,8 +1,8 @@
 module.exports = {
   gcodemetadata: require('./gcodemetadata'),
   gcoderesponse: require('./gcoderesponse'),
-  klipperstates: require('./klipperstates'),
   klipperstate: require('./klipperstate'),
+  klipperstates: require('./klipperstates'),
   klipperstatus_state_update: require('./klipperstatus_state_update'),
   klipperstatus_state: require('./klipperstatus_state'),
   klipperstatus_vsdcard: require('./klipperstatus_vsdcard'),
