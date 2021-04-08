f7f9977b1b2999f7bd5ed401cb5dd63b2b64b6ce
eliteSCHW31N <business@eliteschw31n.de>
2021-04-08 23:32:50 +0200

    add template

--- utils/hsComponents/disk.js
+++ utils/hsComponents/disk.js
@@ -1,5 +1,23 @@
 const si = require('systeminformation')
 
+const template = {
+    "Disk ${diskindex} Name": {
+        "value":  "${disk.device}"
+    },
+    "Disk ${diskindex} Type": {
+        "value":  "${disk.type}"
+    },
+    "Disk ${diskindex} Model": {
+        "value":  "${disk.name}"
+    },
+    "Disk ${diskindex} Vendor": {
+        "value":  "${disk.vendor}"
+    },
+    "Disk ${diskindex} Used": {
+        "value":  "${disk.size}"
+    },
+}
+
 module.exports = {}
 module.exports.getTitle = () => { return 'Disks' }
 module.exports.getFields = async () => {
@@ -10,6 +28,26 @@ module.exports.getFields = async () => {
     for (const diskindex in disks) {
         
         const disk = disks[diskindex]
+
+        const stringTemplate = JSON.stringify(template)
+
+        const translatedTemplate = stringTemplate
+            .replace(/(\${diskindex})/g, diskindex)
+            .replace(/(\${disk.device})/g, disk.device)
+            .replace(/(\${disk.type})/g, disk.type)
+            .replace(/(\${disk.name})/g, disk.name)
+            .replace(/(\${disk.vendor})/g, disk.vendor)
+            .replace(/(\${disk.size})/g, `${(partition.size / (1024 ** 3)).toFixed(2)}GB`)
+        
+        const translatedJSONTemplate = JSON.parse(translatedTemplate)
+        
+        for (const index in translatedJSONTemplate) {
+            fields.push({
+                name: index,
+                value: translatedJSONTemplate[index].value,
+                inline: true
+            })
+        }
 
         if (!String(disk.device).includes('/dev/ram')) {
             fields.push({
