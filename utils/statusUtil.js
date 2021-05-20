89068b7750ca65b94f200cd2f3d8c3b4527a8965
eliteSCHW31N <39838619+eliteSchwein@users.noreply.github.com>
2021-05-20 16:22:59 +0200

    update debug

--- utils/statusUtil.js
+++ utils/statusUtil.js
@@ -115,6 +115,8 @@ function postStatus(message, altdiscordClient, altdatabase) {
           const channel = await client.channels.fetch(guilddatabase.broadcastchannels[index])
           const lastMessageID = channel.lastMessageID
           const lastMessage = await channel.messages.fetch(lastMessageID)
+          console.log(lastMessage.author.id + " " + client.id)
+          console.log(lastMessage.embeds)
           if (lastMessage.author.id === client.id &&
             lastMessage.embeds.length > 0) {
               const embed = lastMessage.embeds[0]
