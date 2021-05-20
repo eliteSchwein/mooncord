71d9b04bbffe24810587e70485a6b8f3acd668c7
eliteSCHW31N <business@eliteschw31n.de>
2021-05-20 19:11:45 +0200

    update debug

--- utils/webcamUtil.js
+++ utils/webcamUtil.js
@@ -19,7 +19,7 @@ function retrieveWebcam () {
         image.rotate(config.rotation)
         image.mirror(config.horizontal_mirror, config.vertical_mirror)
         console.log(await image.getBase64Async(Jimp.MIME_PNG))
-        buffer = Buffer.from(await image.getBase64Async, 'base64')
+        //buffer = Buffer.from(await image.getBase64Async, 'base64')
         return new Discord.MessageAttachment(buffer, 'snapshot.png')
       }
     )
