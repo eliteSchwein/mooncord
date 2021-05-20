c67a98b7d58f76219d2c9e2664fcf444493c3f8d
eliteSCHW31N <business@eliteschw31n.de>
2021-05-20 17:56:57 +0200

    fix below 0

--- utils/miscUtil.js
+++ utils/miscUtil.js
@@ -4,10 +4,11 @@ const database = require('./databaseUtil')
 module.exports.init = () => {
   if (statusconfig.use_percent) {
     setInterval(() => {
-        const ramDatabase = database.getRamDatabase()
+      const ramDatabase = database.getRamDatabase()
       const currentTime = ramDatabase.cooldown
-      console.log("time "+currentTime)
-        database.updateRamDatabase("cooldown", currentTime-1)
+      if (currentTime > 0) {
+        database.updateRamDatabase("cooldown", currentTime - 1)
+      }
     }, 1000)
   }
 }
