176d99fa8c8cdacbe28da367839b2f557f2b7853
eliteSCHW31N <39838619+eliteSchwein@users.noreply.github.com>
2021-05-20 16:24:15 +0200

    fix first check

--- utils/statusUtil.js
+++ utils/statusUtil.js
@@ -115,9 +115,9 @@ function postStatus(message, altdiscordClient, altdatabase) {
           const channel = await client.channels.fetch(guilddatabase.broadcastchannels[index])
           const lastMessageID = channel.lastMessageID
           const lastMessage = await channel.messages.fetch(lastMessageID)
-          console.log(lastMessage.author.id + " " + client.id)
+          console.log(lastMessage.author.id + " " + client.getClient().id)
           console.log(lastMessage.embeds)
-          if (lastMessage.author.id === client.id &&
+          if (lastMessage.author.id === client.getClient().id &&
             lastMessage.embeds.length > 0) {
               const embed = lastMessage.embeds[0]
               console.log(embed)
