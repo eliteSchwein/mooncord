81a4333b93521d299d9b4bcdb0708ae0f0821a1f
eliteSCHW31N <admin@eliteschw31n.de>
2021-05-27 09:43:12 +0200

    add broadcast gcode command handler

--- websocket-events/gcodecommands.js
+++ websocket-events/gcodecommands.js
@@ -2,7 +2,7 @@ const config = require('../statusconfig.json')
 const status = require('../utils/statusUtil')
 const variables = require('../utils/variablesUtil')
 
-const event = (message, connection, discordClient) => {
+const event = (message, connection, discordClient, database) => {
   const id = Math.floor(Math.random() * parseInt('10_000')) + 1
   if (message.type === 'utf8') {
     const messageJson = JSON.parse(message.utf8Data)
@@ -10,7 +10,15 @@ const event = (message, connection, discordClient) => {
     if (typeof (methode) !== 'undefined' && methode === 'notify_gcode_response') {
       const { params } = messageJson
       if(params[0].startsWith("mooncord.broadcast")) {
-        console.log(params[0].replace("mooncord.broadcast ", ""))
+        const message = params[0].replace("mooncord.broadcast ", "")
+        const broadcastembed = new Discord.MessageEmbed()
+          .setColor('#fcf803')
+          .setTitle('Message')
+          .attachFiles(path.resolve(__dirname, '../images/update.png'))
+          .setThumbnail('attachment://update.png')
+          .setTimestamp()
+          .setDescription(message)
+        status.postBroadcastMessage(broadcastembed, discordClient, database)
       }
     }
   }
