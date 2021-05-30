ccafe0c270ae5ea36b78b46d9b7a658b6df18ccb
eliteSCHW31N <business@eliteschw31n.de>
2021-05-30 21:34:58 +0200

    add migrate script

--- scripts/migrate.sh
+++ scripts/migrate.sh
@@ -4,32 +4,6 @@ SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 MCPATH="$( pwd -P )"
 MCCONFIGPATH=""
 
-install_packages()
-{
-    echo "Update package data"
-    sudo apt update
-
-    echo "Install needed packages"
-    sudo apt-get -y install nano git
-
-    if ! command -v node -v >/dev/null 2>&1
-    then
-        echo "Download Node 11"
-        wget https://nodejs.org/download/release/v11.15.0/node-v11.15.0-linux-armv6l.tar.gz
-
-        echo "Install Node 11.15.0"
-        tar -xvf node-v11.15.0-linux-armv6l.tar.gz >/dev/null 2>&1 
-        sudo cp -R node-v11.15.0-linux-armv6l/* /usr/local/ >/dev/null 2>&1 
-
-        echo "Remove Node File and Folder"
-        rm -rf node-v11.15.0-linux-armv6l.tar.gz
-        rm -rf node-v11.15.0-linux-armv6l
-    fi
-
-    echo "Install Dependencies"
-    npm ci --only=prod
-}
-
 install_systemd_service()
 {
     echo "Installing MoonCord unit file"
@@ -50,14 +24,10 @@ install_systemd_service()
     sudo systemctl enable MoonCord
 }
 
-modify_user()
-{
-    sudo usermod -a -G tty $USER
-}
-
 setup(){
     locate_config
     generate_config
+    migrate_config
 }
 
 
@@ -85,14 +55,18 @@ generate_config()
     cp $SCRIPTPATH/database.json $MCPATH/database.json
 }
 
+migrate_config()
+{
+    echo "Migrate Configs"
+    npm run migrate2 "$MCPATH/" $MCCONFIGPATH
+}
+
 start_MoonCord() {
 
     echo "Start MoonCord, please make sure you configured the Bot correctly!"
     sudo systemctl start MoonCord
 }
 
-install_packages
-modify_user
 setup
 install_systemd_service
 start_MoonCord
