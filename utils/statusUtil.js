73e7b40eb867075ec0714bb9359d5c9a462c4af5
eliteSCHW31N <business@eliteschw31n.de>
2021-05-31 19:55:32 +0200

    add debug

--- utils/statusUtil.js
+++ utils/statusUtil.js
@@ -104,6 +104,8 @@ async function generateEmbed(config, user) {
 
 function postStatus(message, altdiscordClient, altdatabase) {
 
+  console.log(config)
+
   const client = getDiscordClient(altdiscordClient)
 
   const maindatabase = getCurrentDatabase(altdatabase)
