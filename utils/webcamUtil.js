923d6d42311fc8e6dea7a8515cdb04696fe06b65
eliteSCHW31N <business@eliteschw31n.de>
2021-06-02 18:25:51 +0200

    debug axios data

--- utils/webcamUtil.js
+++ utils/webcamUtil.js
@@ -17,22 +17,16 @@ function retrieveWebcam() {
     })
     .then(
       async (response) => {
+        console.log(response.data)
         const buffer = Buffer.from(response.data, 'base64')
         const image = await jimp.read(buffer)
-        console.log(0)
         image.quality(config.webcam.quality)
-        console.log(1)
         image.rotate(config.webcam.rotation)
-        console.log(2)
         image.mirror(config.webcam.horizontal_mirror, config.webcam.vertical_mirror)
-        console.log(3)
         image.contrast(config.webcam.contrast)
-        console.log(4)
         image.brightness(config.webcam.brightness)
-        console.log(5)
         if (config.webcam.greyscale) {
           image.greyscale()
-        console.log("5.1")
         }
         const editbuffer = await image.getBufferAsync(jimp.MIME_PNG)
         return new Discord.MessageAttachment(editbuffer, 'snapshot.png')
