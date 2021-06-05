e132cf06902ed1b94c99b76c13eedfb9cdfce394
eliteSCHW31N <business@eliteschw31n.de>
2021-06-05 20:49:50 +0200

    add timelapse command

---
+++ discord/dynamicCommands/timelapse.js
@@ -0,0 +1,48 @@
+const { SlashCommand } = require('slash-create')
+const Discord = require('discord.js')
+const variablesUtil = require('../../utils/variablesUtil')
+
+async function generateEmbed() {
+    const embed = new Discord.MessageEmbed()
+        .setColor('#0099ff')
+        .setTitle(`Timelapse of ${variablesUtil.getLastGcodeFile()}`)
+        .setAuthor(variablesUtil.getLastGcodeFile())
+        .attachFiles('./temp/timelapse/timelapse.gif')
+    
+    return embed
+}
+
+module.exports = class HelloCommand extends SlashCommand {
+    constructor(creator) {
+        super(creator, {
+            name: 'timelapse',
+            description: 'Get the latest Timelapse.'
+        })
+        this.filePath = __filename
+    }
+
+    async run(ctx) {
+        try {
+            if (variablesUtil.getLastGcodeFile() === '') {
+                return "There is no Thumbnail aviable!"
+            }
+            const embed = await generateEmbed()
+
+            const timelapse = embed.files[0]
+
+            const files = {
+                name: timelapse.name,
+                file: timelapse.attachment
+            }
+
+            await ctx.send({
+                file: files,
+                embeds: [embed.toJSON()]
+            })
+        }
+        catch (error) {
+            console.log((error).error)
+            return "An Error occured!"
+        }
+    }
+}
