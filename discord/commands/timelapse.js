6f58572c0113a53d32f57f9dded9edf40c5be994
eliteSCHW31N <business@eliteschw31n.de>
2021-06-07 13:52:15 +0200

    delete temporarly code

--- discord/commands/timelapse.js
+++ discord/commands/timelapse.js
@@ -1,7 +1,6 @@
 const { SlashCommand } = require('slash-create')
 const Discord = require('discord.js')
 const config = require(`${args[0]}/mooncord.json`)
-const variablesUtil = require('../../utils/variablesUtil')
 
 module.exports = class HelloCommand extends SlashCommand {
     constructor(creator) {
@@ -14,36 +13,11 @@ module.exports = class HelloCommand extends SlashCommand {
 
     async run(ctx) {
         try {
-            if (variablesUtil.getLastGcodeFile() === '') {
-                return "There is no Thumbnail aviable!"
-            }
-            const embed = await generateEmbed()
-
-            const timelapse = embed.files[0]
-
-            const files = {
-                name: timelapse.name,
-                file: timelapse.attachment
-            }
-
-            await ctx.send({
-                file: files,
-                embeds: [embed.toJSON()]
-            })
+            
         }
         catch (error) {
             console.log((error).error)
             return "An Error occured!"
         }
     }
-}
-
-async function generateEmbed() {
-    const embed = new Discord.MessageEmbed()
-        .setColor('#0099ff')
-       // .setTitle(`Timelapse of ${variablesUtil.getLastGcodeFile()}`)
-       // .setAuthor(variablesUtil.getLastGcodeFile())
-        .attachFiles('./temp/timelapse/timelapse.gif')
-    
-    return embed
 }
