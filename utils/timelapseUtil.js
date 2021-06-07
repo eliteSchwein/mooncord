12938c7b8493a2ece1e7b1300c4604ef26725ab3
eliteSCHW31N <business@eliteschw31n.de>
2021-06-07 14:57:08 +0200

    fix buffer getter

--- utils/timelapseUtil.js
+++ utils/timelapseUtil.js
@@ -45,10 +45,9 @@ async function makeFrame() {
         return
     }
     const snapshot = await webcamUtil.retrieveWebcam()
-    console.log(snapshot)
-    fs.writeFile(path.resolve(__dirname, `../temp/timelapse/frame-${framecount}.png`), snapshot, 'base64', function(err) {
-        console.log(err);
-    })
+    const frame = await fs.writeFileSync(path.resolve(__dirname,
+        `../temp/timelapse/frame-${framecount}.png`),
+        snapshot.attachment, 'base64')
     frames.push(`../temp/timelapse/frame-${framecount}.png`)
     framecount ++
 }
