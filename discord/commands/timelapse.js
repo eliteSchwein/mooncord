16b0d98633d109311a75b4e052b65c1a4e159522
eliteSCHW31N <business@eliteschw31n.de>
2021-06-07 13:52:56 +0200

    copy paste info command

--- discord/commands/timelapse.js
+++ discord/commands/timelapse.js
@@ -1,19 +1,44 @@
-const { SlashCommand } = require('slash-create')
 const Discord = require('discord.js')
-const config = require(`${args[0]}/mooncord.json`)
+const fs = require('fs')
+const path = require('path')
+const { SlashCommand } = require('slash-create')
+
+const pjson = require('../../package.json')
 
 module.exports = class HelloCommand extends SlashCommand {
     constructor(creator) {
         super(creator, {
             name: 'timelapse',
-            description: 'Get the latest Timelapse.'
+            description: 'Send a Description about me.'
         })
         this.filePath = __filename
     }
 
     async run(ctx) {
         try {
-            
+            const description = `Version: ${pjson.version}\n
+            Author: ${pjson.author}\n
+            Homepage: ${pjson.homepage}\n`
+        
+            const logopath = path.resolve(__dirname, '../../images/logo.png')
+
+            const logobuffer = fs.readFileSync(logopath)
+
+            const infoEmbed = new Discord.MessageEmbed()
+                .setColor('#0099ff')
+                .setTitle('Informations')
+                .setDescription(description)
+                .setThumbnail('attachment://logo.png')
+        
+            ctx.defer(false)
+
+            await ctx.send({
+                file: {
+                    name: 'logo.png',
+                    file: logobuffer
+                },
+                embeds: [infoEmbed.toJSON()]
+            })
         }
         catch (error) {
             console.log((error).error)
