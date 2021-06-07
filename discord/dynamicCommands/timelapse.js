32a4a6d4b898af25f550a60b8ebeb61914739b35
eliteSCHW31N <business@eliteschw31n.de>
2021-06-07 14:17:08 +0200

    change default value to undefinied

--- discord/dynamicCommands/timelapse.js
+++ discord/dynamicCommands/timelapse.js
@@ -7,7 +7,7 @@ const config = require(`${args[0]}/mooncord.json`)
 
 module.exports = class HelloCommand extends SlashCommand {
     constructor(creator) {
-        let guildId = ''
+        let guildId = undefined
         if (!config.thumbnail.enable) {
             guildId = '000000000000000000'
         }
