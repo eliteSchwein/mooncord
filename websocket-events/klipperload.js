54d1121d78612fe37c8bf4917f44e188500fa273
eliteSCHW31N <business@eliteschw31n.de>
2021-06-12 17:20:09 +0200

    debug mcu output

--- websocket-events/klipperload.js
+++ websocket-events/klipperload.js
@@ -4,10 +4,10 @@ const variables = require('../utils/variablesUtil')
 const event = (message, connection, discordClient) => {
   if (message.type === 'utf8') {
     const messageJson = JSON.parse(message.utf8Data)
-    console.log(messageJson)
     if (typeof (messageJson.result) === 'undefined') { return }
     if (typeof (messageJson.result.status) === 'undefined') { return }
     console.log(messageJson.result.status.system_stats)
+    console.log(messageJson.result.status.mcu)
   }
 }
 module.exports = event
