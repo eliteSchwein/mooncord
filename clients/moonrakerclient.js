57d45a2637131a0834c65cfbcf89c18e4cb223b1
eliteSCHW31N <business@eliteschw31n.de>
2021-06-12 17:53:49 +0200

    stringify json

--- clients/moonrakerclient.js
+++ clients/moonrakerclient.js
@@ -44,7 +44,7 @@ const enableEvents = function (discordClient) {
         const mculist = variables.getMCUList()
         connection.send(`{"jsonrpc": "2.0", "method": "machine.update.status", "params": {"refresh": "false"}, "id": ${id}}`)
         connection.send(`{"jsonrpc": "2.0", "method": "printer.objects.query", "params": {"objects": {"webhooks": null, "virtual_sdcard": null, "print_stats": null, "gcode_move": null, "system_stats": null }}, "id": ${id}}`)
-        connection.send(`{"jsonrpc": "2.0", "method": "printer.objects.query", "params": {"objects": ${mculist}}, "id": ${id}}`)
+        connection.send(`{"jsonrpc": "2.0", "method": "printer.objects.query", "params": {"objects": ${JSON.stringify(mculist)}}, "id": ${id}}`)
       }, 250)
     }, 250)
 
