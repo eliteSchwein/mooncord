550528c9b4cefc2ed17a52ad2f84869756d80e2b
eliteSCHW31N <business@eliteschw31n.de>
2021-06-12 17:16:06 +0200

    get correct values

--- websocket-events/klipperload.js
+++ websocket-events/klipperload.js
@@ -4,7 +4,9 @@ const variables = require('../utils/variablesUtil')
 const event = (message, connection, discordClient) => {
   if (message.type === 'utf8') {
     const messageJson = JSON.parse(message.utf8Data)
-    console.log(messageJson)
+    if (typeof (messageJson.result) === 'undefined') { return }
+    if (typeof (messageJson.result.status) === 'undefined') { return }
+    console.log(messageJson.result.status.system_stats)
   }
 }
 module.exports = event
