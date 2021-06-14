3d01766e1aee91fdff2d27e35eda8e075f822df6
eliteSCHW31N <admin@eliteschw31n.de>
2021-06-14 14:20:39 +0200

    remove debug

--- utils/localeUtil.js
+++ utils/localeUtil.js
@@ -2,5 +2,4 @@ const args = process.argv.slice(2)
 const config = require(`${args[0]}/mooncord.json`)
 const locale = require(`../locales/${config.language}.json`)
 
-console.log(locale)
 module.exports = locale
