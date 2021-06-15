5adff7a004c514618a07d710629479b43e371624
eliteSCHW31N <business@eliteschw31n.de>
2021-06-15 21:32:35 +0200

    remove debug

--- utils/thumbnailUtil.js
+++ utils/thumbnailUtil.js
@@ -11,13 +11,15 @@ const path = require('path')
 
 async function retrieveThumbnail (url) {
   let thumbnail
+
   await getBase64(`${statusconfig.connection.moonraker_url}/server/files/gcodes/${url}`).then((buffer) => {
     thumbnail = buffer
-    console.log(buffer)
   })
+
   if (typeof (thumbnail) === 'undefined' || thumbnail === '') {
     return new Discord.MessageAttachment(await fs.readFile(path.resolve(__dirname, '../images/thumbnail_not_found.png')), 'thumbnail_not_found.png')
   }
+  
   const buffer = Buffer.from(thumbnail, 'base64')
   return new Discord.MessageAttachment(buffer, 'thumbnail.png')
 }
