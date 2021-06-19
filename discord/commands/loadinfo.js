0dacd090ce371a5b1e3cfd36a06b95add6381a73
eliteSCHW31N <business@eliteschw31n.de>
2021-06-19 18:46:25 +0200

    update type check

--- discord/commands/loadinfo.js
+++ discord/commands/loadinfo.js
@@ -60,7 +60,7 @@ async function retrieveMCUComponent(mcu) {
 
     const mcudata = variablesUtil.getMCUList()[mcu]
 
-    if (typeof (mcudata) === 'undefined') {
+    if (typeof (mcudata.last_stats) === 'undefined') {
         const description = locale.errors.no_data
         embed.setDescription(description)
         embed.setColor('#c90000')
