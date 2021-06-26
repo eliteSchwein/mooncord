7567711844fb252530a56d44b66504dec1e0a383
eliteSCHW31N <business@eliteschw31n.de>
2021-06-26 20:37:12 +0200

    use correct filter, debug output

--- websocket-events/moonrakerThrottle.js
+++ websocket-events/moonrakerThrottle.js
@@ -3,9 +3,9 @@ const variables = require('../utils/variablesUtil')
 const event = (message, connection, discordClient) => {
   if (message.type === 'utf8') {
     const messageJson = JSON.parse(message.utf8Data)
-    console.log(messageJson)
     if (typeof (messageJson.result) === 'undefined') { return }
-    if (typeof (messageJson.result.status) === 'undefined') { return }
+    if (typeof (messageJson.result.moonraker_stats) === 'undefined') { return }
+    console.log(messageJson.result.moonraker_stats)
   }
 }
 module.exports = event
