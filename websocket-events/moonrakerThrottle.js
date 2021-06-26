5736d08e4ce1aa3163483715d98cb453b3eafefb
eliteSCHW31N <business@eliteschw31n.de>
2021-06-26 20:39:41 +0200

    implement methodes

--- websocket-events/moonrakerThrottle.js
+++ websocket-events/moonrakerThrottle.js
@@ -4,8 +4,18 @@ const event = (message, connection, discordClient) => {
   if (message.type === 'utf8') {
     const messageJson = JSON.parse(message.utf8Data)
     if (typeof (messageJson.result) === 'undefined') { return }
-    if (typeof (messageJson.result.moonraker_stats) === 'undefined') { return }
-    console.log(messageJson.result.moonraker_stats)
+    if (typeof (messageJson.result.moonraker_stats) !== 'undefined') {
+      retrieveStats(messageJson.result.moonraker_stats)
+    }
+    if (typeof (messageJson.result.throttled_state) !== 'undefined') {
+      retrieveThrottle(messageJson.result.throttled_state)
+    }
   }
+}
+function retrieveStats(result) {
+  console.log(result)
+}
+function retrieveThrottle(result) {
+  console.log(result)
 }
 module.exports = event
