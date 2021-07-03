0a432bbe6df16c90272d865e782e31ec13418a51
eliteSCHW31N <business@eliteschw31n.de>
2021-07-03 17:36:00 +0200

    update order

--- websocket-events/gcodeMetaData.js
+++ websocket-events/gcodeMetaData.js
@@ -10,15 +10,15 @@ const event = (message) => {
 
     console.log(result)
 
-    if (typeof (result.thumbnails) !== 'undefined') {
-        variables.setThumbnailPath(result.thumbnails[1].relative_path)
-    }
-
     variables.setStartByte(result.gcode_start_byte)
     variables.setEndByte(result.gcode_end_byte)
     variables.setPrintTime(result.estimated_time)
     variables.setLayerHeights(result.layer_height,
       result.object_height, result.first_layer_height)
+    
+    if (typeof (result.thumbnails) !== 'undefined') {
+      variables.setThumbnailPath(result.thumbnails[1].relative_path)
+    }
   }
 }
 module.exports = event
