64042583f7527f9dacf43ae79a8305b154ae9ff3
eliteSCHW31N <business@eliteschw31n.de>
2021-07-03 22:23:59 +0200

    add async

--- websocket-events/klipperState.js
+++ websocket-events/klipperState.js
@@ -1,7 +1,7 @@
 const status = require('../utils/statusUtil')
 const variables = require('../utils/variablesUtil')
 
-const event = (message, connection, discordClient) => {
+const event = async (message, connection, discordClient) => {
   if (message.type === 'utf8') {
     const messageJson = JSON.parse(message.utf8Data)
     const { result } = messageJson
@@ -13,7 +13,7 @@ const event = (message, connection, discordClient) => {
     if (variables.getStatus() === currentStatus) { return }
       console.log('klipperState ' + currentStatus)
     variables.setStatus(currentStatus)
-    status.triggerStatusUpdate(discordClient)
+    await status.triggerStatusUpdate(discordClient)
   }
 }
 module.exports = event
