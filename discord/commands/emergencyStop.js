890030be203c88fc93542924616d76ee10762d81
eliteSCHW31N <business@eliteschw31n.de>
2021-07-09 20:24:03 +0200

    fix emergency Stop command

--- discord/commands/emergencyStop.js
+++ discord/commands/emergencyStop.js
@@ -1,6 +1,7 @@
 const logSymbols = require('log-symbols')
 const { SlashCommand } = require('slash-create')
 
+const discordClient = require('../../clients/discordClient')
 const moonrakerClient = require('../../clients/moonrakerClient')
 const locale = require('../../utils/localeUtil')
 const permission = require('../../utils/permissionUtil')
