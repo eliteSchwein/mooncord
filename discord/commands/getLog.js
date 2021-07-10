4cacddb9e3f3d6b88782da916be524493cfebf2e
eliteSCHW31N <business@eliteschw31n.de>
2021-07-10 15:59:28 +0200

    fix some buggs in getlog command

--- discord/commands/getLog.js
+++ discord/commands/getLog.js
@@ -25,7 +25,7 @@ module.exports = class EditChannelCommand extends SlashCommand {
                 type: CommandOptionType.STRING,
                 name: syntaxLocale.options.log_file.name,
                 description: messageLocale.options.log_file.description,
-                required: false
+                required: true
             }]
         })
         this.filePath = __filename
@@ -43,6 +43,7 @@ module.exports = class EditChannelCommand extends SlashCommand {
         const logFile = getLog(service)
 
         return ({
+            content: '',
             file: {
                 name: `${service}.log`,
                 file: logFile
