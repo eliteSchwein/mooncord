b176c93ffec9fe1dc0566632a7cd541570399c6f
eliteSCHW31N <business@eliteschw31n.de>
2021-07-11 20:23:51 +0200

    debug users

--- utils/statusUtil.js
+++ utils/statusUtil.js
@@ -147,7 +147,8 @@ function postStatus(message, altdiscordClient, altdatabase) {
 async function removeOldStatus(channel, discordClient) {
   const lastMessage = await channel.messages.fetch({ limit: 1 })
 
-  console.log(discordClient)
+  console.log(discordClient.user)
+  console.log(lastMessage.author)
 
   if (lastMessage.author.id !== discordClient.user.id) { return }
   if (lastMessage.embeds.size === 0) { return }
