86ec28443f5883624aaf74792613c835dc24fc04
eliteSCHW31N <business@eliteschw31n.de>
2021-07-11 20:20:59 +0200

    remove wrong function

--- utils/statusUtil.js
+++ utils/statusUtil.js
@@ -145,8 +145,7 @@ function postStatus(message, altdiscordClient, altdatabase) {
 }
 
 async function removeOldStatus(channel, discordClient) {
-  console.log(channel)
-  const lastMessage = await channel.messages.fetch({ limit: 1 }).first()
+  const lastMessage = await channel.messages.fetch({ limit: 1 })
 
   if (lastMessage.author.id !== discordClient.user.id) { return }
   if (lastMessage.embeds.size === 0) { return }
