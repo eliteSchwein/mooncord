ebd54c052cb6ad544736073ee08baf8b42661d09
eliteSCHW31N <business@eliteschw31n.de>
2021-07-15 11:37:40 +0200

    move progress calculation

--- websocket-events/klipperDisplayStatus.js
+++ websocket-events/klipperDisplayStatus.js
@@ -1,6 +1,11 @@
-const variables = require('../utils/variablesUtil')
+const args = process.argv.slice(2)
 
-const event = (message) => {
+const variables = require('../utils/variablesUtil')
+const statusUtil = require('../utils/statusUtil')
+
+const config = require(`${args[0]}/mooncord.json`)
+
+const event = (message, connection, discordClient) => {
   if (message.type !== 'utf8') { return }
 
   const messageJson = JSON.parse(message.utf8Data)
@@ -13,5 +18,24 @@ const event = (message) => {
   const progress = result.status.display_status.progress
 
   variables.updateTimeData('file_total_duration', variables.getTimes().duration / progress)
+
+  postProgress(discordClient, progress)
+  
+  variables.setProgress((progress * 100).toFixed())
+}
+
+function postProgress(discordClient, progress) {
+  if (variables.getProgress() === progress) { return }
+  
+  discordClient.user.setActivity(
+    locale.status.printing.activity.replace(/(\${value_print_progress})/g, variables.getProgress())
+    , { type: 'WATCHING' })
+  
+
+  if (config.status.update_interval &&
+    progress.toFixed() % config.status.update_interval === 0 &&
+    progress.toFixed(0) !== 0) {
+      status.changeStatus(discordClient, 'printing')
+  }
 }
 module.exports = event
--- websocket-events/klipperStatusVirtualSDCard.js
+++ websocket-events/klipperStatusVirtualSDCard.js
@@ -1,15 +1,6 @@
-const args = process.argv.slice(2)
-
-const { waitUntil } = require('async-wait-until')
-
-const config = require(`${args[0]}/mooncord.json`)
-const locale = require('../utils/localeUtil')
-const status = require('../utils/statusUtil')
 const variables = require('../utils/variablesUtil')
 
-let lastProgress = 0
-
-const event = async (message, connection, discordClient) => {
+const event = async (message) => {
   if (message.type !== 'utf8') { return }
   const messageJson = JSON.parse(message.utf8Data)
   const { result } = messageJson
@@ -17,30 +8,6 @@ const event = async (message, connection, discordClient) => {
   retrieveSoftwareVersion(result)
 
   retrieveGGodeMove(result)
-
-  if (hasCorrectData(result)) {
-    const virtualSDcard = result.status.virtual_sdcard
-    const currentProgress = calculateProgress(virtualSDcard)
-
-    if (status.getStatus() !== 'printing') { return }
-    
-    if (currentProgress.toFixed(0) === variables.getProgress()) { return }
-
-    variables.setProgress(currentProgress.toFixed(0))
-    
-    discordClient.user.setActivity(
-      locale.status.printing.activity.replace(/(\${value_print_progress})/g, currentProgress.toFixed(0))
-      , { type: 'WATCHING' })
-
-    if (currentProgress.toFixed(0) === lastProgress) { return }
-
-    if (config.status.update_interval &&
-      currentProgress.toFixed(0) % config.status.update_interval === 0 &&
-      currentProgress.toFixed(0) !== 0) {
-      lastProgress = currentProgress.toFixed(0)
-      status.changeStatus(discordClient, 'printing')
-    }
-  }
 }
 
 function retrieveGGodeMove(result) {
@@ -60,30 +27,4 @@ function retrieveSoftwareVersion(result) {
   variables.setVersions(result.version_info)
 }
 
-function hasCorrectData(result) {
-  if (typeof (result) === 'undefined') { return false }
-  if (typeof (result.status) === 'undefined') { return false }
-  if (typeof (result.status.virtual_sdcard) === 'undefined') { return false }
-  return true
-}
-
-function calculateProgress(virtual_sdcard) {
-  let currentProgress = 0
-  if (virtual_sdcard.file_position <= variables.getStartByte()) {
-    currentProgress = 0
-    variables.setProgress(currentProgress)
-  } else if (virtual_sdcard.file_position >= variables.getEndByte()) {
-    currentProgress = 100
-    variables.setProgress(currentProgress)
-  } else {
-    const currentPosition = virtual_sdcard.file_position - variables.getStartByte()
-    const maxPosition = variables.getEndByte() - variables.getStartByte()
-    if (currentPosition > 0 && maxPosition > 0) {
-      currentProgress = (1 / maxPosition * currentPosition) * 100
-    } else {
-      currentProgress = virtual_sdcard.progress
-    }
-  }
-  return currentProgress
-}
 module.exports = event
