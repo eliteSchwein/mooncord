1931b71e4807b688ca8874f4f7260ca6ad615776
eliteSCHW31N <business@eliteschw31n.de>
2021-07-15 15:28:52 +0200

    debug check

--- websocket-events/klipperDisplayStatus.js
+++ websocket-events/klipperDisplayStatus.js
@@ -27,6 +27,7 @@ const event = (message, connection, discordClient) => {
 
 function postProgress(discordClient, progress) {
   if (variables.getProgress() === progress) { return }
+  console.log(progress === 100)
   if (progress === 100) { return }
   
   discordClient.user.setActivity(
