5fb2ddc348d08e18d2096280151d91cd08a6cb5a
eliteSCHW31N <business@eliteschw31n.de>
2021-07-27 22:00:12 +0200

    add printJob buttons

--- discord/buttons/printJob.js
+++ discord/buttons/printJob.js
@@ -1,8 +1,36 @@
+const permission = require('../../utils/permissionUtil')
+const locale = require('../../utils/localeUtil')
+const statusUtil = require('../../utils/statusUtil')
+const moonrakerClient = require('../../clients/moonrakerClient')
+
 const metaData = require('../buttons-metadata/print_job.json')
+
+const messageLocale = locale.commands.printjob
 
 module.exports = async (button, discordClient) => {
     const message = button.message
+    const user = button.clicker.user
+
     if (message.author.id !== discordClient.user.id) { return }
     if (!Object.keys(metaData).includes(button.id)) { return }
-    console.log(button)
+    if (!await permission.hasAdmin(user, button.guild.id, discordClient)) {
+        button.reply.send(message.channel.send(locale.getAdminOnlyError(user.username)))
+    }
+
+    const currentStatus = statusUtil.getStatus()
+    const buttonMeta = metaData[key]
+    const langCommandMeta = messageLocale.answer[key]
+
+    if (subcommand === currentStatus) {
+        button.reply.send(langCommandMeta.status_same.replace(/(\${username})/g, user.username))
+    }
+
+    if (!buttonMeta.requiredStatus.includes(currentStatus)) {
+        button.reply.send(langCommandMeta.status_not_valid.replace(/(\${username})/g, user.username))
+    }
+
+    const connection = moonrakerClient.getConnection()
+    const id = Math.floor(Math.random() * Number.parseInt('10_000')) + 1
+
+    connection.send(`{"jsonrpc": "2.0", "method": "printer.gcode.script", "params": {"script": "${buttonMeta.macro}"}, "id": ${id}}`)
 }
