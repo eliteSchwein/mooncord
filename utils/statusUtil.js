e84027edfbaecdaf3521369b49e28ebe52b61682
eliteSCHW31N <admin@eliteschw31n.de>
2021-07-28 09:30:18 +0200

    debug feedback channel

--- utils/statusUtil.js
+++ utils/statusUtil.js
@@ -114,7 +114,8 @@ async function broadcastSection(list, section, discordClient, message) {
     channel = await discordClient[section].fetch(id)
 
     await removeOldStatus(channel, discordClient)
-    console.log(await channel.send(message))
+    const feedback = await channel.send(message)
+    console.log(feedback.channel)
   }
 }
 
