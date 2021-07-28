7fa574ddd4c5d0281015c3714bade52a661277b8
eliteSCHW31N <admin@eliteschw31n.de>
2021-07-28 15:48:54 +0200

    update imports

--- discord/buttons/index.js
+++ discord/buttons/index.js
@@ -1,9 +1,11 @@
 const printJob = require('./printJob')
 const klipperRestart = require('./klipperRestart')
 const updateSystem = require('./updateSystem')
+const listFiles = require('./listFiles')
 
 module.exports = (button, discordClient) => {
   printJob(button, discordClient)
   klipperRestart(button, discordClient)
   updateSystem(button, discordClient)
+  listFiles(button, discordClient)
 }
