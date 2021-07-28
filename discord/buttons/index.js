135f0e6731993fdf4e165450194fb0e750b2df08
eliteSCHW31N <business@eliteschw31n.de>
2021-07-28 19:17:00 +0200

    add printjob no button handler

--- discord/buttons/index.js
+++ discord/buttons/index.js
@@ -1,10 +1,12 @@
 const printJob = require('./printJob')
+const printJobStart = require('./printJobStart')
 const klipperRestart = require('./klipperRestart')
 const updateSystem = require('./updateSystem')
 const listFiles = require('./listFiles')
 
 module.exports = (button, discordClient) => {
   printJob(button, discordClient)
+  printJobStart(button, discordClient)
   klipperRestart(button, discordClient)
   updateSystem(button, discordClient)
   listFiles(button, discordClient)
---
+++ discord/buttons/printJobStart.js
@@ -0,0 +1,25 @@
+const moonrakerClient = require('../../clients/moonrakerClient')
+const permission = require('../../utils/permissionUtil')
+const locale = require('../../utils/localeUtil')
+
+module.exports = async (button, discordClient) => {
+    const message = button.message
+    const user = button.clicker.user
+
+    if (message.author.id !== discordClient.user.id) { return }
+
+    let guildID
+
+    if(typeof(button.guild) !== 'undefined') { guildID = button.guild.id }
+
+    if (!await permission.hasAdmin(user, guildID, discordClient)) {
+        button.reply.send(message.channel.send(locale.getAdminOnlyError(user.username)))
+    }
+    switch (button.id) {
+        case ("printjob_start_no"): {
+            const abortMessage = commandlocale.answer.abort.replace(/(\${username})/g, user.username)
+            await message.edit({ abortMessage, buttons: undefined })
+            await message.suppressEmbeds(true)
+        }
+    }
+}
