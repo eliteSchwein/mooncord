8b2953faeab6fa11d7b4f4d06084694eefd42c1c
eliteSCHW31N <business@eliteschw31n.de>
2021-07-28 19:59:47 +0200

    remove old printJobStart Handler

--- discord/events/index.js
+++ discord/events/index.js
@@ -1,10 +1,8 @@
-const printJobStart = require('./printJobStart')
 const upload = require('./upload')
 const buttonClick = require('./buttonClick')
 
 const enableEvent = function (discordClient) {
   upload(discordClient)
-  printJobStart(discordClient)
   buttonClick(discordClient)
 }
 module.exports = enableEvent
