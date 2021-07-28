b013af518b7229d1e14b1b241c5967ec36636f14
eliteSCHW31N <admin@eliteschw31n.de>
2021-07-28 09:26:19 +0200

    add permissions

--- discord/buttons/updateSystem.js
+++ discord/buttons/updateSystem.js
@@ -1,10 +1,17 @@
 const moonrakerClient = require('../../clients/moonrakerClient')
+const permission = require('../../utils/permissionUtil')
+const locale = require('../../utils/localeUtil')
 
 module.exports = async (button, discordClient) => {
     const message = button.message
+    const user = button.clicker.user
 
     if (message.author.id !== discordClient.user.id) { return }
     if (button.id !== 'update_system') { return }
+
+    if (!await permission.hasAdmin(user, guildID, discordClient)) {
+        button.reply.send(message.channel.send(locale.getAdminOnlyError(user.username)))
+    }
 
     const connection = moonrakerClient.getConnection()
     const id = Math.floor(Math.random() * Number.parseInt('10_000')) + 1
