1fb55d301a36c7db675374d1084f31ba024e1e1d
eliteSCHW31N <admin@eliteschw31n.de>
2021-07-28 15:46:42 +0200

    remove test button handler

--- discord/buttons/index.js
+++ discord/buttons/index.js
@@ -1,10 +1,8 @@
-const test = require('./test')
 const printJob = require('./printJob')
 const klipperRestart = require('./klipperRestart')
 const updateSystem = require('./updateSystem')
 
 module.exports = (button, discordClient) => {
-  test(button, discordClient)
   printJob(button, discordClient)
   klipperRestart(button, discordClient)
   updateSystem(button, discordClient)
--- discord/buttons/test.js
+++
@@ -1,5 +0,0 @@
-module.exports = async (button, discordClient) => {
-    const message = button.message
-    if (message.author.id !== discordClient.user.id) { return }
-    console.log(button)
-}
