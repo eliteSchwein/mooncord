daee479ac11199c24d260788a08dddafd552ffba
eliteSCHW31N <admin@eliteschw31n.de>
2021-07-28 08:15:40 +0200

    debug channel

--- utils/statusUtil.js
+++ utils/statusUtil.js
@@ -85,6 +85,7 @@ function onCooldown(config, isSame) {
 async function removeOldStatus(channel, discordClient) {
   if(channel === null) { return }
   if(typeof(channel) === 'undefined') { return }
+  console.log(channel)
   let lastMessage = await channel.messages.fetch({ limit: 1 })
   lastMessage = lastMessage.first()
 
