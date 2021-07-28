ac90c93a262af65e7d26a54b46015737e7bfe832
eliteSCHW31N <business@eliteschw31n.de>
2021-07-28 19:56:54 +0200

    add start message and handler

--- discord/buttons/printJobStart.js
+++ discord/buttons/printJobStart.js
@@ -21,8 +21,23 @@ module.exports = async (button, discordClient) => {
     switch (button.id) {
         case ("printjob_start_no"): {
             await message.edit({ embed: getAbortEmbed(message, user), components: null })
+            return
+        }
+        case ("printjob_start_yes"): {
+            await message.edit({ embed: getStartEmbed(message, user), components: null })
+            //startPrint(message)
+            return
         }
     }
+}
+
+function startPrint(currentMessage) {
+    const currentEmbed = currentMessage.embeds[0]
+    const gcodeFile = currentEmbed.author.name
+    const connection = moonrakerClient.getConnection()
+    const id = Math.floor(Math.random() * Number.parseInt('10_000')) + 1
+
+    connection.send(`{"jsonrpc": "2.0", "method": "printer.print.start", "params": {"filename": "${gcodeFile}"}, "id": ${id}}`)
 }
 
 function getAbortEmbed(currentMessage, user) {
@@ -33,4 +48,14 @@ function getAbortEmbed(currentMessage, user) {
         .setAuthor(currentEmbed.author.name, 'attachment://thumbnail.png')
         .setDescription(abortMessage)
     return abortEmbed
+}
+
+function getStartEmbed(currentMessage, user) {
+    const currentEmbed = currentMessage.embeds[0]
+    const startMessage = commandlocale.answer.executed.replace(/(\${username})/g, user.username)
+    const startEmbed = new Discord.MessageEmbed()
+        .setColor('#25db00')
+        .setAuthor(currentEmbed.author.name, 'attachment://thumbnail.png')
+        .setDescription(startMessage)
+    return startEmbed
 }
