931e369a971738ef04ce68e46392a6d94272faa9
eliteSCHW31N <business@eliteschw31n.de>
2021-08-10 21:20:14 +0200

    use correct methode

--- discord/commands/index.js
+++ discord/commands/index.js
@@ -1,7 +1,7 @@
 const emergencyStop = require('./emergencyStop')
 
-module.exports.addCommandEvents = async (discordClient) => { await loadSlashCommands(discordClient) }
-module.exports.loadSlashCommands = (discordClient) => {}
+module.exports.addCommandEvents = (discordClient) => { }
+module.exports.loadSlashCommands = async (discordClient) => { await loadSlashCommands(discordClient) }
 
 async function loadSlashCommands(discordClient) {
     const commands = []
