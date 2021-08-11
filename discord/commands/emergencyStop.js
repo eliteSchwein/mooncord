96d5837abb983ed1ba3095c16484104ce6fd605a
eliteSCHW31N <business@eliteschw31n.de>
2021-08-11 21:57:43 +0200

    remove useless import

--- discord/commands/emergencyStop.js
+++ discord/commands/emergencyStop.js
@@ -1,6 +1,5 @@
 const logSymbols = require('log-symbols')
 
-const discordClient = require('../../clients/discordClient')
 const moonrakerClient = require('../../clients/moonrakerClient')
 const locale = require('../../utils/localeUtil')
 const permission = require('../../utils/permissionUtil')
