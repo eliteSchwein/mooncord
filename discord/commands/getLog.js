3ff19c228eb6954a0c8035d65e6f96ab50ddb07d
eliteSCHW31N <business@eliteschw31n.de>
2021-08-16 20:40:11 +0200

    use other parser

--- discord/commands/getLog.js
+++ discord/commands/getLog.js
@@ -15,7 +15,7 @@ const syntaxLocale = locale.syntaxlocale.commands.get_log
 
 module.exports.command = () => {
     console.log('  Load Get Log Command'.commandload)
-    console.log(Object.entries(metadata.choices))
+    console.log(Object.values(metadata.choices))
     const command = new SlashCommandBuilder()
         .setName(syntaxLocale.command)
         .setDescription(messageLocale.description)
