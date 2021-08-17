cfa8b7618d655a6b285fa5a39b75e8147b765638
eliteSCHW31N <business@eliteschw31n.de>
2021-08-17 22:28:28 +0200

    remove debug

--- discord/commands/listFiles.js
+++ discord/commands/listFiles.js
@@ -76,7 +76,6 @@ module.exports.reply = async (interaction) => {
 
 async function handler (message) {
     const messageJson = JSON.parse(message.utf8Data)
-        console.log(messageJson)
     if(/(modified)/g.test(JSON.stringify(messageJson))) {
         connection.removeListener('message', handler)
         commandFeedback = await chatUtil.generatePageEmbed(
