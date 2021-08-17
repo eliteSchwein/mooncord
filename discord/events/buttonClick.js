ac40a2d3c6815dd127d5a25d2a2f540f71b048a1
eliteSCHW31N <business@eliteschw31n.de>
2021-08-17 20:47:21 +0200

    update button event

--- discord/events/buttonClick.js
+++ discord/events/buttonClick.js
@@ -1,8 +1,10 @@
 const handlers = require('../buttons')
 
 const enableEvent = (discordClient) => {
-  discordClient.on('clickButton', (button) => {
-    handlers(button, discordClient)
+  discordClient.on('interactionCreate', (interaction) => {
+    if (!interaction.isButton()) return
+    console.log(interaction)
+    //handlers(interaction, discordClient)
   })
 }
 module.exports = enableEvent
