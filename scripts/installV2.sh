6a7e1110c526f872ea6ebb1b5a5a728a87748d3f
eliteSCHW31N <business@eliteschw31n.de>
2021-08-18 20:56:22 +0200

    debug value

--- scripts/installV2.sh
+++ scripts/installV2.sh
@@ -69,7 +69,7 @@ locate_config()
 
         KEY=$(echo $ARGUMENT | cut -f1 -d=)
         VALUE=$(echo $ARGUMENT | cut -f2 -d=)   
-
+        echo $VALUE
         case "$KEY" in
                 --config_path) MCCONFIGPATH=${VALUE} ;;
                 --service_suffix) MCSERVICENAME="${MCSERVICENAME}_${VALUE}" ;;     
