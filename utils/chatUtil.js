288497f739fcf46e927875a222af175c7acfd865
eliteSCHW31N <admin@eliteschw31n.de>
2021-08-19 09:17:17 +0200

    update loop forumlar

--- utils/chatUtil.js
+++ utils/chatUtil.js
@@ -149,7 +149,7 @@ module.exports.generatePageEmbed = (pageUp, currentPage, data, title, icon, addF
     }
   }
   let entries = '\n'
-  for (let i = (newpage * maxEntries) + newpage; i == maxEntries + (newpage * maxEntries) + newpage; i++) {
+  for (let i = (newpage * maxEntries) + newpage; i <= maxEntries + (newpage * maxEntries) + newpage - 1; i++) {
     if (i < data.length) {
       entries = entries.concat(`${data[i].path}\n`)
     }
