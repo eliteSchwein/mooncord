06dc44ac9c452e074ba3731248691a09eca4bea0
eliteSCHW31N <business@eliteschw31n.de>
2021-08-26 21:39:23 +0200

    remove useless function

--- websocket-events/moonrakerUpdate.js
+++ websocket-events/moonrakerUpdate.js
@@ -22,67 +22,31 @@ function retrieveSoftwareVersion(result) {
   if (typeof (result) === 'undefined') { return }
   if (typeof (result.version_info) === 'undefined') { return }
 
-  const diffVersions = {}
-  for (const software in result.version_info) {
-    const softwareinfo = result.version_info[software]
-    const difference = getDifference(software, softwareinfo)
-    if (typeof (difference) !== 'undefined') {
-      diffVersions[software] = difference
-    }
-  }
   variables.setVersions(result.version_info)
-  postUpdate(diffVersions, discordClient, database)
-}
-
-function getDifference(software, softwareinfo) {
-  if (software === 'system') {
-    if (softwareinfo.package_count === 0) { return }
-
-    let oldPackageAmount = 0
-
-    if (Object.keys(variables.getVersions()).length > 0) {
-      oldPackageAmount = variables.getVersions()[software].package_count
-    }
-
-    if (softwareinfo.package_count !== oldPackageAmount) {
-      return {
-        'packages': softwareinfo.package_count
-      }
-    }
-  } else {
-    if (softwareinfo.version === softwareinfo.remote_version) { return }
-
-    let oldVersionData = ''
-
-    if (typeof (variables.getVersions()[software]) !== 'undefined') {
-      oldVersionData = variables.getVersions()[software].remote_version
-    }
-    
-    if (softwareinfo.remote_version !== oldVersionData) {
-      return {
-        'current': softwareinfo.version,
-        'remote': softwareinfo.remote_version
-      }
-    }
-  }
+  postUpdate(result.version_info, discordClient, database)
 }
 
 async function postUpdate(updateData, discordClient, database) {
   if (Object.keys(updateData).length === 0) { return }
+
   console.log(logSymbols.info, `There are some Updates!`.printstatus)
+
   const notifyEmbed = new Discord.MessageEmbed()
     .setColor('#fcf803')
     .setTitle(locale.update.title)
     .setThumbnail('attachment://update.png')
     .setTimestamp()
+  
   const icon = new Discord.MessageAttachment(path.resolve(__dirname, '../images/update.png'))
+
   for (const software in updateData) {
     if (software === 'system') {
-      notifyEmbed.addField(locale.update.system, `${locale.update.packages}: ${updateData[software].packages}`, true)
+      notifyEmbed.addField(locale.update.system, `${locale.update.packages}: ${updateData[software].package_count}`, true)
     } else {
-      notifyEmbed.addField(software, `${updateData[software].current} \n🆕 ${updateData[software].remote}`, true)
+      notifyEmbed.addField(software, `${updateData[software].version} \n🆕 ${updateData[software].remote_version}`, true)
     }
   }
+  
   const row = new Discord.MessageActionRow()
   
   const button = new Discord.MessageButton()
