602f775d984902d5e745a78ba72ac6b729e1dda2
eliteSCHW31N <admin@eliteschw31n.de>
2021-08-26 15:09:02 +0200

    add functions

--- websocket-eventsV2/objectHandlers/mcu.js
+++ websocket-eventsV2/objectHandlers/mcu.js
@@ -1,8 +1,29 @@
+const variables = require('../utils/variablesUtil')
+
 module.exports = (data, connection, discordClient, database) => {
-  Object.keys(data).forEach(key => {
-    if (!/(temp)/g.test(key) && 
-        /(mcu)/g.test(key)) {
-      console.log(key)
+    loopMCUs(data)
+}
+
+function loopMCUs(data) {
+    Object.keys(data).forEach(mcu => {
+      if (!/(temp)/g.test(mcu) && 
+          /(mcu)/g.test(mcu)) {
+          updateMCU(mcu, data[mcu])
+      }
+    })
+}
+
+function updateMCU(mcu, data) {
+    if(typeof(variables.getMCUList()[mcu]) === 'undefined') {
+        variables.updateMCU(mcu, data)
+        return
     }
-  })
+
+    const oldMcuData = variables.getMCUList()[mcu]
+
+    oldMcuData.last_stats.mcu_task_avg = data.last_stats.mcu_task_avg
+    oldMcuData.last_stats.mcu_awake = data.last_stats.mcu_awake
+    oldMcuData.last_stats.freq = data.last_stats.freq
+    
+    variables.updateMCU(mcu, data)
 }
