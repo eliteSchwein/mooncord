9615cf4af92b267610b7de512875e80b425567a9
eliteSCHW31N <admin@eliteschw31n.de>
2021-08-26 09:36:50 +0200

    remove debug

--- websocket-events/klipperLoad.js
+++ websocket-events/klipperLoad.js
@@ -18,7 +18,6 @@ const event = (message) => {
 function retrieveMCUStatus(message) {
   
   Object.keys(message).forEach(key => {
-    console.log(key)
     if (!/(temp)/g.test(key) && 
         /(mcu)/g.test(key)) {
       variables.updateMCUStatus(key, message[key])
