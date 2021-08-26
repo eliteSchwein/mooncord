f885c6b21cc21cd3eb946fcafbfd13a74900ba46
eliteSCHW31N <admin@eliteschw31n.de>
2021-08-26 16:50:54 +0200

    debug progress

--- websocket-eventsV2/objectHandlers/displayStatus.js
+++ websocket-eventsV2/objectHandlers/displayStatus.js
@@ -10,6 +10,8 @@ module.exports = (data, connection, discordClient, database) => {
 
     const {progress} = data.display_status
 
+    console.log(progress)
+
     variables.updateTimeData('file_total_duration', variables.getTimes().duration / progress)
   
     postProgress(discordClient, (progress * 100).toFixed(0))
