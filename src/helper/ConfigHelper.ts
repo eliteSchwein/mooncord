50a7f51180d6ffab7d0fc9e9ea39c625ff574f70
eliteSCHW31N <business@eliteschw31n.de>
2021-09-18 19:55:01 +0200

    add: confighelper

---
+++ src/helper/ConfigHelper.ts
@@ -0,0 +1,101 @@
+import {readFileSync} from 'fs'
+
+const args = process.argv.slice(2)
+
+export class ConfigHelper {
+    protected configPath = `${args[0]}/mooncord.json`
+    protected configRaw = readFileSync(this.configPath, {encoding: 'utf8'})
+    protected config = JSON.parse(this.configRaw)
+
+    public constructor() {
+        console.log(this.config)
+    }
+
+    public getMoonrakerSocketUrl() {
+        return this.config.connection.moonraker_socket_url
+    }
+
+    public getMoonrakerUrl() {
+        return this.config.connection.moonraker_url
+    }
+
+    public getMoonrakerApiKey() {
+        return this.config.connection.moonraker_token
+    }
+
+    public getDiscordToken() {
+        return this.config.connection.bot_token
+    }
+
+    public getStatusInterval() {
+        return this.config.status.update_interval
+    }
+
+    public getStatusMinInterval() {
+        return this.config.status.min_interval
+    }
+
+    public isStatusPerPercent() {
+        return this.config.status.use_percent
+    }
+
+    public getStatusBeforeTasks() {
+        return this.config.status.before
+    }
+
+    public getStatusAfterTasks() {
+        return this.config.status.before
+    }
+
+    public getLanguage() {
+        return this.config.language.messages
+    }
+
+    public getSyntaxLanguage() {
+        return this.config.language.command_syntax
+    }
+
+    public getController() {
+        return this.config.permission.controller
+    }
+
+    public isGuildAdminAsBotAdmin() {
+        return this.config.permission.guild_admin_as_bot_admin
+    }
+
+    public getWebcamUrl() {
+        return this.config.webcam.url
+    }
+
+    public getWebcamQuality() {
+        return this.config.webcam.quality
+    }
+
+    public getWebcamBrightness() {
+        return this.config.webcam.brightness
+    }
+
+    public getWebcamContrast() {
+        return this.config.webcam.contrast
+    }
+
+    public isWebcamVerticalMirrored() {
+        return this.config.webcam.vertical_mirror
+    }
+
+    public isWebcamHorizontalMirrored() {
+        return this.config.webcam.horizontal_mirror
+    }
+
+    public isWebcamGreyscale() {
+        return this.config.webcam.greyscale
+    }
+
+    public isWebcamSepia() {
+        return this.config.webcam.sepia
+    }
+
+    public notifyOnMoonrakerThrottle() {
+        return this.config.system_notifications.moonraker_throttle
+    }
+}
