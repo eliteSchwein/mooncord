764f46844c1d08eb34856bb86b332a9a9baaaaa5
eliteSCHW31N <admin@eliteschw31n.de>
2021-10-19 09:27:20 +0200

    update: add fields title to status Generator

--- src/generator/DiscordStatusGenerator.ts
+++ src/generator/DiscordStatusGenerator.ts
@@ -25,6 +25,14 @@ export class DiscordStatusGenerator {
             })
         }
 
+        const fieldAssign = JSON.stringify(tempCache)
+            .replace(/(\${locale.print_time})/g, locale.fields.print_time)
+            .replace(/(\${locale.print_layers})/g, locale.fields.print_layers)
+            .replace(/(\${locale.eta_print_time})/g, locale.fields.eta_print_time)
+            .replace(/(\${locale.print_progress})/g, locale.fields.print_progress)
+
+        mergeDeep(tempCache, JSON.parse(fieldAssign))
+
         setData('status_messages', tempCache)
     }
 }
