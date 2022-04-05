24ca0d2434ecb48a6df1d139af77ae0461a32ede
eliteSCHW31N <business@eliteschw31n.de>
2022-04-05 23:12:27 +0200

    add chromium install

--- scripts/install.sh
+++ scripts/install.sh
@@ -105,7 +105,7 @@ install_packages()
     sudo apt update
 
     status_msg "Install needed packages"
-    sudo apt-get -y install git
+    sudo apt-get -y install --no-install-recommends git libgbm1 libxkbcommon0 chromium
 
     if ! command -v node -v >/dev/null 2>&1
     then
