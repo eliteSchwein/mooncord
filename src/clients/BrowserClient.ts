d02a4c449d5a7c4b37248bdfa938c8516bf64201
eliteSCHW31N <business@eliteschw31n.de>
2022-04-11 21:14:08 +0200

    add puppeteer on compile ignore list

--- src/clients/BrowserClient.ts
+++ src/clients/BrowserClient.ts
@@ -1,10 +1,12 @@
 import type {Browser} from 'puppeteer';
 import Puppeteer from 'puppeteer'
+import {logRegular} from "../helper/LoggerHelper";
 
 export class BrowserClient {
     protected browser: Browser
 
     public async initBrowser() {
+        logRegular('init Browser Client...')
         this.browser = await Puppeteer.launch({
             defaultViewport: null,
             args: ['--no-sandbox', '--incognito'],
