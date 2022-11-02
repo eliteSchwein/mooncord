b7025333d192ef439f38ba829214c01e18123b35
eliteSCHW31N <admin@eliteschw31n.de>
2022-11-02 18:48:03 +0100

    feat: add custom command functions and message

--- src/events/discord/interactions/commands/CustomCommand.ts
+++ src/events/discord/interactions/commands/CustomCommand.ts
@@ -1,15 +1,20 @@
 import {CommandInteraction, MessageAttachment} from "discord.js";
 import * as CacheUtil from "../../../../utils/CacheUtil";
 import * as path from "path";
-import {getDatabase} from "../../../../Application";
+import {getDatabase, getMoonrakerClient} from "../../../../Application";
 import {LocaleHelper} from "../../../../helper/LocaleHelper";
 import {DiscordCommandGenerator} from "../../../../generator/DiscordCommandGenerator";
+import {ConsoleHelper} from "../../../../helper/ConsoleHelper";
+import {logRegular, logWarn} from "../../../../helper/LoggerHelper";
 
 export class CustomCommand {
     protected commandGenerator = new DiscordCommandGenerator()
     protected databaseUtil = getDatabase()
     protected localeHelper = new LocaleHelper()
+    protected locale = this.localeHelper.getLocale()
     protected syntaxLocale = this.localeHelper.getSyntaxLocale()
+    protected consoleHelper = new ConsoleHelper()
+    protected moonrakerClient = getMoonrakerClient()
 
     public constructor(interaction: CommandInteraction, commandId: string) {
         if(!this.commandGenerator.isCustomCommand(commandId)) { return }
@@ -19,7 +24,27 @@ export class CustomCommand {
 
     protected async execute(interaction: CommandInteraction, commandId: string) {
         const customCommandData = this.commandGenerator.getCustomCommandData(commandId)
-        console.log(customCommandData)
-        await interaction.reply(JSON.stringify(customCommandData))
+
+        await interaction.deferReply()
+
+        if(customCommandData.macros !== undefined) {
+            await this.consoleHelper.executeGcodeCommands(customCommandData.macros,
+                interaction.channel)
+        }
+
+        if(customCommandData.websocket_commands !== undefined) {
+            for(const websocketCommand of customCommandData.websocket_commands) {
+                logRegular(`Execute Websocket Command ${websocketCommand}...`)
+                try {
+                    await this.moonrakerClient.send(websocketCommand)
+                } catch {
+                    logWarn(`The Websocket Command ${websocketCommand} timed out...`)
+                }
+            }
+        }
+
+        await interaction.editReply(this.locale.messages.answers.custom_command_executed
+            .replace(/\${username}/g, interaction.user.tag)
+            .replace(/\${custom_command}/g, commandId))
     }
 }
