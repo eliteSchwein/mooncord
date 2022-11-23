5fce025e9b17a9f09f6153a47d57ea260741acef
eliteSCHW31N <admin@eliteschw31n.de>
2022-11-23 09:31:30 +0100

    feat: generate svg chart with polylines

--- src/helper/GraphHelper.ts
+++ src/helper/GraphHelper.ts
@@ -62,6 +62,8 @@ ${svg}
     public async getTempGraph(sensor = undefined) {
         const moonrakerClient = App.getMoonrakerClient()
         const chartConfigSection = this.configHelper.getGraphConfig('temp_history')
+        const tempLabel = this.locale.graph.temp_history.temperature
+        const powerLabel = this.locale.graph.temp_history.power
 
         this.tempValueLimit = chartConfigSection.value_limit
 
@@ -132,182 +134,34 @@ ${svg}
             }
         }
 
-        const chartConfig = {
-            'type': 'line',
-            'data': {
-                'datasets': [],
-                'labels': []
-            },
-            'options': {
-                'layout': {
-                    'padding': {
-                        'right': 1
-                    }
-                },
-                'elements': {
-                    'point':{
-                        'radius': 0
-                    }
-                },
-                'legend': {
-                    'display': false,
-                    'labels': {
-                        'fontSize': 14,
-                        'fontColor': 'rgb(255,255,255)'
-                    }
-                },
-                'title': {
-                    'display': false
-                },
-                'animation': {
-                    'duration': 0
-                },
-                'hover': {
-                    'animationDuration': 0
-                },
-                'responsiveAnimationDuration': 0,
-                'scales': {
-                    'xAxes': [
-                        {
-                            'drawTicks': false,
-                            'color': 'rgba(255, 255, 255, 0)'
-                        }
-                    ],
-                    'yAxes': [
-                        {
-                            'id': 'temp',
-                            'type': 'linear',
-                            'position': 'left',
-                            'color': 'rgba(255, 255, 255, 0)',
-                            'ticks': {
-                                'min': 0,
-                                'stepSize': 0.5,
-                                'fontSize': 20,
-                                'maxTicksLimit': 12
-                            },
-                            'scaleLabel': {
-                                'fontSize': 20,
-                                'display': true,
-                                'labelString': this.locale.graph.temp_history.temperature
-                            }
-                        }
-                    ]
-                }
+        let svg = `<svg
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlns:xlink="http://www.w3.org/1999/xlink"
+            viewBox="0 0 ${width} ${resHeight}">
+            <g transform="translate(200,0)">
+            `
+
+        for(const line of lines) {
+            if(line.coords === undefined) {
+                continue
             }
+            svg = `
+                ${svg}
+                <polyline points="${line.coords.join(' ')}" style="fill:none;stroke:${line.color};stroke-width:5" />
+            `
         }
+        svg =`
+            ${svg}
+            </g>
+            `
 
-        if(typeof tempHistoryRequest.error !== 'undefined') {
-            return
-        }
-
-        for(const rawTempSensor in tempHistoryRequest.result) {
-            const tempSensor = rawTempSensor.replace(/(temperature_sensor )|(temperature_fan )|(heater_generic )/g, '')
-
-            if(typeof sensor !== 'undefined' && tempSensor !== sensor) { continue }
-
-            const tempValues = this.getTempValues(tempHistoryRequest.result[rawTempSensor].temperatures)
-            const tempTargets = this.getTempValues(tempHistoryRequest.result[rawTempSensor].targets)
-            const tempPowers =  this.getTempValues(tempHistoryRequest.result[rawTempSensor].powers)
-
-            chartConfig.data.datasets.push({
-                'label': tempSensor,
-                'borderColor': this.tempCache.colors[rawTempSensor].color,
-                'fill': false,
-                'data': tempValues,
-                'yAxisID': 'temp'
-            })
-
-            if(typeof sensor !== 'undefined') {
-                chartConfig.options.legend.display = true
-
-                chartConfig.options.scales.yAxes.push({
-                    'id': 'power',
-                    'color': 'rgba(255, 255, 255, 0)',
-                    'type': 'linear',
-                    'position': 'right',
-                    'ticks': {
-                        // @ts-ignore
-                        'max': 100,
-                        'min': 0,
-                        'fontSize': 20
-                    },
-                    'scaleLabel': {
-                        'fontSize': 20,
-                        'display': true,
-                        'labelString': this.locale.graph.temp_history.power
-                    }
-                })
-
-                const parsedTempPowers = []
-
-                for(let i = 0; i < tempPowers.length; i++) {
-                    parsedTempPowers.push(tempPowers[i] * 100)
-                }
-
-                chartConfig.data.datasets.push({
-                    'label': `${tempSensor}_power`,
-                    'lineStyle': {
-                        'type': 'dashed'
-                    },
-                    'borderColor': this.tempCache.colors[rawTempSensor].color,
-                    'backgroundColor': 'rgba(0,0,0,0)',
-                    'data': parsedTempPowers,
-                    'borderDash': [5,10],
-                    'yAxisID': 'power'
-                })
-            }
-
-            chartConfig.data.datasets.push({
-                'label': `${tempSensor}_target`,
-                'backgroundColor': this.tempCache.colors[rawTempSensor].color+'35',
-                'borderColor': this.tempCache.colors[rawTempSensor].color+'00',
-                'data': tempTargets,
-                'yAxisID': 'temp'
-            })
-        }
-
-        for(let i = 0; i < this.tempValueLimit; i++) {
-           chartConfig.data.labels.push('')
-        }
-
-        const chart = await this.renderChart(chartConfig, 800, 400, 'temp')
-
-        if(typeof chart === 'undefined') {
-            return
-        }
-
-        return chart
-    }
-
-    private async renderChart(chartConfig, width: number, height: number, chartName: string) {
-        logRegular(`Render the Chart for ${chartName}...`)
-        const quickChart = new QuickChart()
-
-        quickChart
-            .setConfig(chartConfig)
-            .setHeight(height)
-            .setWidth(width)
-            .setBackgroundColor('#000000')
-
-        const url = await quickChart.getShortUrl()
-
-        await sleep(500)
-
-        return url
-    }
-
-    private getTempValues(tempValues: []) {
-        if(typeof tempValues === 'undefined') {
-            return []
-        }
-
-        if(tempValues.length < this.tempValueLimit) {
-            return tempValues
-        }
-
-        const limitStart = tempValues.length - this.tempValueLimit
-
-        return tempValues.slice(limitStart)
+        svg = `
+            ${svg}
+            </svg>
+        `
+        const graphBuffer = await sharp(Buffer.from(svg)).png().toBuffer()
+        return new MessageAttachment(graphBuffer, 'tempGraph.png')
     }
 
     private convertToCoords(values: [], max: number, resHeight = 400) {
