1d41f0b6a8eb66ad0a4dfb65105f31a5877c7091
eliteSCHW31N <admin@eliteschw31n.de>
2023-04-11 08:59:03 +0200

    feat: prepare history graph icons parser

--- src/helper/graphs/HistoryGraph.ts
+++ src/helper/graphs/HistoryGraph.ts
@@ -7,7 +7,14 @@ export default class HistoryGraph {
     public getIcons() {
         const icons = {}
 
-        console.log(metaData)
+        for(const iconKey of metaData.icons) {
+            const iconData = this.configHelper.getIcons(new RegExp(`${iconKey}`, 'g'))
+
+            console.log(iconKey)
+        }
+
+        console.log('history graph icons')
+        console.log(metaData.icons)
 
         return this.configHelper.getIcons(/temp\d+$/g)
     }
