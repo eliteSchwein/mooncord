6d8b8cc4f8e27f9b435d8366f8d3c6c8ff09d1b6
eliteSCHW31N <admin@eliteschw31n.de>
2023-07-06 09:12:51 +0200

    feat: migrate migration script to moonraker webcam setup

--- src/migration/ConfigMigration.js
+++ src/migration/ConfigMigration.js
@@ -19,38 +19,10 @@ console.log('update base config...')
 
 newConfig =
     newConfig
-        .replace('MC_WEBCAM_URL', legacyConfig.webcam.url)
         .replace(/moonraker_url:\s*.*/g, `moonraker_url: '${legacyConfig.connection.moonraker_url}'`)
         .replace(/moonraker_token:\s*.*/g, `moonraker_token: '${legacyConfig.connection.moonraker_token}'`)
         .replace(/bot_token:\s*.*/g, `bot_token: '${legacyConfig.connection.bot_token}'`)
         .replace(/tmp_path:\s*.*/g, `tmp_path: '${legacyConfig.tmp_path}'`)
-
-console.log('update detailes cam settings...')
-
-if(legacyConfig.webcam.vertical_mirror !== undefined) {
-    newConfig =
-        newConfig.replace(/vertical_mirror:\s*.*/g, `vertical_mirror: ${legacyConfig.webcam.vertical_mirror}`)
-}
-
-if(legacyConfig.webcam.horizontal_mirror !== undefined) {
-    newConfig =
-        newConfig.replace(/horizontal_mirror:\s*.*/g, `horizontal_mirror: ${legacyConfig.webcam.horizontal_mirror}`)
-}
-
-if(legacyConfig.webcam.rotation !== undefined) {
-    newConfig =
-        newConfig.replace(/rotate:\s*.*/g, `rotate: ${legacyConfig.webcam.rotation}`)
-}
-
-if(legacyConfig.webcam.brightness !== undefined) {
-    newConfig =
-        newConfig.replace(/brightness:\s*.*/g, `brightness: ${legacyConfig.webcam.brightness}`)
-}
-
-if(legacyConfig.webcam.contrast !== undefined) {
-    newConfig =
-        newConfig.replace(/contrast:\s*.*/g, `contrast: ${legacyConfig.webcam.contrast}`)
-}
 
 console.log('write new config, keeping old json config as backup...')
 
