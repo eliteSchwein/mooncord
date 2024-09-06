9a7e7dff7a2a473a97106da27332f0884188ece0
Thomas Ludwig <business@tludwig.dev>
2024-09-06 10:01:33 +0200

    feat: prepare multi gcode upload

--- src/events/discord/GCodeUploadHandler.ts
+++ src/events/discord/GCodeUploadHandler.ts
@@ -1,6 +1,6 @@
 'use strict'
 
-import {Client, MessageEmbed} from "discord.js";
+import {Client, Message, MessageAttachment, MessageEmbed} from "discord.js";
 import {getMoonrakerClient} from "../../Application";
 import {ConfigHelper} from "../../helper/ConfigHelper";
 import {LocaleHelper} from "../../helper/LocaleHelper";
@@ -22,13 +22,6 @@ export class GCodeUploadHandler {
                 return
             }
 
-            const attachment = message.attachments.at(0)
-            const fileName = attachment.name
-
-            if (!fileName.endsWith('.gcode')) {
-                return
-            }
-
             const permissionHelper = new PermissionHelper()
 
             if (!permissionHelper.hasPermission(message.author, message.guild, 'gcode_upload')) {
@@ -36,33 +29,59 @@ export class GCodeUploadHandler {
                 return;
             }
 
-            const localeHelper = new LocaleHelper()
-            const embedHelper = new EmbedHelper()
-            const locale = localeHelper.getLocale()
-
             await message.channel.sendTyping()
 
-            const uploadRequest = await uploadAttachment(attachment)
-
-            if (uploadRequest) {
-                const metaDataHelper = new MetadataHelper()
-                const metaData = await metaDataHelper.getMetaData(fileName)
-                const thumbnail = await metaDataHelper.getThumbnail(fileName)
-                const embedData = await embedHelper.generateEmbed('printjob_start_request', metaData)
-                const embed = embedData.embed.embeds[0] as MessageEmbed
-
-                embed.setThumbnail(`attachment://${thumbnail.name}`)
-
-                embedData.embed.embeds = [embed]
-                embedData.embed['files'] = [thumbnail]
-
-                await message.reply(embedData.embed)
+            if(message.attachments.size > 1) {
+                await this.uploadMultipleFiles(message)
                 return
             }
 
-            await message.reply(locale.messages.errors.upload_failed
-                .replace(/(\${filename})/g, attachment.name)
-                .replace(/(\${username})/g, message.author.tag))
+            await this.uploadSingleFile(message)
         })
+    }
+
+    private async uploadSingleFile(message: Message) {
+        const attachment = message.attachments.at(0)
+        const fileName = attachment.name
+
+        if (!fileName.endsWith('.gcode')) {
+            return
+        }
+
+        const localeHelper = new LocaleHelper()
+        const embedHelper = new EmbedHelper()
+        const locale = localeHelper.getLocale()
+
+        const uploadRequest = await uploadAttachment(attachment)
+
+        if (uploadRequest) {
+            const metaDataHelper = new MetadataHelper()
+            const metaData = await metaDataHelper.getMetaData(fileName)
+            const thumbnail = await metaDataHelper.getThumbnail(fileName)
+            const embedData = await embedHelper.generateEmbed('printjob_start_request', metaData)
+            const embed = embedData.embed.embeds[0] as MessageEmbed
+
+            embed.setThumbnail(`attachment://${thumbnail.name}`)
+
+            embedData.embed.embeds = [embed]
+            embedData.embed['files'] = [thumbnail]
+
+            await message.reply(embedData.embed)
+            return
+        }
+
+        await message.reply(locale.messages.errors.upload_failed
+            .replace(/(\${filename})/g, attachment.name)
+            .replace(/(\${username})/g, message.author.tag))
+    }
+
+    private async uploadMultipleFiles(message: Message){
+        for(const attachment of message.attachments) {
+            const fileName = attachment.name
+
+            if (!fileName.endsWith('.gcode')) {
+                continue
+            }
+        }
     }
 }
