78859a0a3b76198766429d5bfe098f2b778b6217
Thomas Ludwig <business@tludwig.dev>
2024-09-17 09:32:02 +0200

    feat: prepare base command

--- src/events/discord/interactions/commands/BaseCommand.ts
+++ src/events/discord/interactions/commands/BaseCommand.ts
@@ -1,3 +1,26 @@
-export default class BaseCommand {
+import {CommandInteraction} from "discord.js";
+import {LocaleHelper} from "../../../../helper/LocaleHelper";
+import {DatabaseUtil} from "../../../../utils/DatabaseUtil";
+import {ConfigHelper} from "../../../../helper/ConfigHelper";
 
+export default class BaseCommand {
+    protected localeHelper = new LocaleHelper()
+    protected locale = this.localeHelper.getLocale()
+    protected syntaxLocale = this.localeHelper.getSyntaxLocale()
+    protected database = new DatabaseUtil()
+    protected config = new ConfigHelper()
+
+    commandId = ''
+
+    public constructor(interaction: CommandInteraction, commandId: string) {
+        if (commandId !== this.commandId) {
+            return
+        }
+
+        void this.handleCommand(interaction)
+    }
+
+    async handleCommand(interaction: CommandInteraction) {
+        // here the command stuff, extend this class
+    }
 }
