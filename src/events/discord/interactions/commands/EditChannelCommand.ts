0f66a8e0ac1a8961295464cb195d24872c676a2d
Thomas Ludwig <business@tludwig.dev>
2024-09-17 18:49:06 +0200

    feat: migrate some commands

---
+++ src/events/discord/interactions/commands/EditChannelCommand.ts
@@ -0,0 +1,67 @@
+import BaseCommand from "./BaseCommand";
+import {ChannelType, ChatInputCommandInteraction, TextChannel} from "discord.js";
+import {removeFromArray} from "../../../../helper/DataHelper";
+
+export default class EditChannelCommand extends BaseCommand {
+    commandId = 'editchannel'
+
+    async handleCommand(interaction: ChatInputCommandInteraction) {
+        const broadcastList = this.database.getDatabaseEntry('guilds')
+
+        let channelOption = interaction.options.getChannel(this.syntaxLocale.commands.editchannel.options.channel.name)
+        const user = interaction.user
+        let channel = interaction.channel
+
+        if (channel === null) {
+            void interaction.reply(this.locale.messages.errors.guild_only
+                .replace(/\${username}/g, user.tag))
+            return
+        }
+
+        // @ts-ignore
+        if (channel.type === ChannelType.DM) {
+            void interaction.reply(this.locale.messages.errors.guild_only
+                .replace(/\${username}/g, user.tag))
+            return
+        }
+
+        if (channelOption !== null) {
+            if (channelOption.type !== ChannelType.GuildText) {
+                void interaction.reply(this.locale.messages.errors.not_textchannel
+                    .replace(/\${username}/g, user.tag)
+                    .replace(/\${channel}/g, channelOption.name))
+                return
+            }
+            channel = channelOption as TextChannel
+        }
+
+        if (!Object.keys(broadcastList).includes(interaction.guildId)) {
+            broadcastList[interaction.guildId] = {
+                'broadcast_channels': []
+            }
+            await this.database.updateDatabaseEntry('guilds', broadcastList)
+        }
+
+        const broadcastChannels = broadcastList[interaction.guildId].broadcast_channels
+
+        let answer
+
+        if (broadcastChannels.includes(channel.id)) {
+            removeFromArray(broadcastChannels, channel.id)
+            answer = this.locale.messages.answers.broadcast_channel.deactivated
+        } else {
+            broadcastChannels.push(channel.id)
+            answer = this.locale.messages.answers.broadcast_channel.activated
+        }
+
+        broadcastList[interaction.guildId].broadcast_channels = broadcastChannels
+
+        answer = answer
+            .replace(/\${username}/g, user.tag)
+            .replace(/\${channel}/g, channel.name)
+
+        await this.database.updateDatabaseEntry('guilds', broadcastList)
+
+        void interaction.reply(answer)
+    }
+}
---
+++ src/events/discord/interactions/commands/EmergencyStopCommand.ts
@@ -0,0 +1,17 @@
+import BaseCommand from "./BaseCommand";
+import {ChatInputCommandInteraction} from "discord.js";
+
+export default class EmergencyStopCommand extends BaseCommand {
+    commandId = 'emergency_stop'
+
+    async handleCommand(interaction: ChatInputCommandInteraction) {
+        await interaction.deferReply()
+
+        void await this.moonrakerClient.send({"method": "printer.emergency_stop"})
+
+        const answer = this.locale.messages.answers.emergency_stop
+            .replace(/\${username}/g, interaction.user.tag)
+
+        await interaction.editReply(answer)
+    }
+}
