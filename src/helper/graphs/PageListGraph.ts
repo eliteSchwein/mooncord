134871e21b145c29b35e4484ad2ccc23db1dbe10
tludwig <t.ludwig@schommer-media.de>
2025-03-31 15:06:16 +0200

    debug

--- src/helper/graphs/PageListGraph.ts
+++ src/helper/graphs/PageListGraph.ts
@@ -15,6 +15,7 @@ export default class PageListGraph extends BaseGraph {
         for(const graphDataEntry of graphData) {
             const graphEntry = graphDataEntry[graphEntryKey]
             console.log(graphDataEntry)
+            console.log(graphEntryKey)
             const graphEntryParameters = []
 
             for(const graphParameter of graphParameters) {
