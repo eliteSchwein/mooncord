c164f876ef735af835b46c8632f0f4907b04ecbe
tludwig <t.ludwig@schommer-media.de>
2025-04-04 16:10:58 +0200

    feat: add option to update cache on websocket handler

--- src/events/discord/interactions/handlers/WebsocketHandler.ts
+++ src/events/discord/interactions/handlers/WebsocketHandler.ts
@@ -4,7 +4,7 @@ import {Message, User} from "discord.js";
 import {getMoonrakerClient} from "../../../../Application";
 import {logRegular, logWarn} from "../../../../helper/LoggerHelper";
 import BaseHandler from "../abstracts/BaseHandler";
-import {getEntry, setData} from "../../../../utils/CacheUtil";
+import {getEntry, setData, updateData} from "../../../../utils/CacheUtil";
 
 export class WebsocketHandler extends BaseHandler {
     async isValid(message: Message, user: User, data, interaction = null) {
@@ -40,13 +40,19 @@ export class WebsocketHandler extends BaseHandler {
         for (let websocketCommand of data.websocket_requests) {
             websocketCommand = await this.templateHelper.parsePlaceholder(JSON.stringify(websocketCommand))
 
+            const updateCache = websocketCommand.update_cache
+
+            delete websocketCommand['update_cache']
+
             logRegular(`Execute Websocket Command ${websocketCommand}...`)
             try {
                 const data = await moonrakerClient.send(JSON.parse(websocketCommand), websocketTimeout)
 
-                console.log(data)
+                if(!data.result || !updateCache) return
+
+                updateData(updateCache, data.result)
             } catch {
-                logWarn(`The Websocket Command ${websocketCommand} timed out...`)
+                logWarn(`The Websocket Command ${websocketCommand} timed out or failed...`)
             }
         }
     }
